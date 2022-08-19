4a4520551ed63eb2d8c6850dae92be111ee886cc
Анатолий <contact@anafro.ru>
2022-08-19 23:03:08 +1200

    Documented Quark class and all its methods

--- src/main/java/ru/anafro/quark/server/api/Quark.java
+++ src/main/java/ru/anafro/quark/server/api/Quark.java
@@ -1,16 +1,23 @@
 package ru.anafro.quark.server.api;
 
+import ru.anafro.quark.server.console.Command;
 import ru.anafro.quark.server.console.CommandArguments;
 import ru.anafro.quark.server.console.CommandLoop;
 import ru.anafro.quark.server.console.CommandRegistry;
 import ru.anafro.quark.server.console.commands.*;
+import ru.anafro.quark.server.console.exceptions.CommandRuntimeException;
+import ru.anafro.quark.server.console.exceptions.CommandSyntaxException;
 import ru.anafro.quark.server.console.exceptions.NoSuchCommandException;
+import ru.anafro.quark.server.databases.exceptions.DatabaseException;
 import ru.anafro.quark.server.databases.ql.Instruction;
 import ru.anafro.quark.server.databases.ql.InstructionArguments;
 import ru.anafro.quark.server.databases.ql.InstructionRegistry;
 import ru.anafro.quark.server.databases.ql.InstructionResult;
+import ru.anafro.quark.server.databases.ql.entities.InstructionEntityConstructor;
 import ru.anafro.quark.server.databases.ql.entities.constructors.*;
+import ru.anafro.quark.server.databases.ql.exceptions.InstructionSyntaxException;
 import ru.anafro.quark.server.databases.ql.instructions.*;
+import ru.anafro.quark.server.databases.views.TableView;
 import ru.anafro.quark.server.exceptions.QuarkException;
 import ru.anafro.quark.server.exceptions.QuarkExceptionHandler;
 import ru.anafro.quark.server.fun.Greeter;
@@ -19,6 +26,15 @@ import ru.anafro.quark.server.multithreading.AsyncServicePool;
 import ru.anafro.quark.server.networking.Server;
 import ru.anafro.quark.server.plugins.PluginManager;
 
+/**
+ * Provides the easiest way of communicating with the Quark Server by having
+ * all Quark Server components in one place. Quark class also contains some
+ * static methods to do the most frequent things a simpler way, such as
+ * running console commands or database instructions.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ */
 public final class Quark {
     private static final Server server = new Server();
     private static final Logger logger = new Logger(Quark.class);
@@ -30,10 +46,29 @@ public final class Quark {
     private static final AsyncServicePool pool = new AsyncServicePool(commandLoop, server);
     private static boolean initialized = false;
 
+    /**
+     * This private constructor of Quark class <strong>MUST NOT</strong> be ever
+     * called, because Quark is a utility class. Use static methods declared inside.
+     *
+     * @see Quark
+     */
     private Quark() {
         //
     }
-
+    /**
+     * Initializes the whole Quark Server with all modules inside. It registers
+     * all default console commands, Quark QL instructions and constructors.
+     * It also loads up the plugins from the /Plugins folder.
+     * <br>
+     * Due to the fact that this method is invoked by Quark Server itself,
+     * <strong>you don't have to invoke it inside your plugins</strong>.
+     *
+     * @throws QuarkException when this method is called twice.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see Quark
+     */
     public static void init() {
         Greeter.greet();
 
@@ -108,30 +143,121 @@ public final class Quark {
         initialized = true;
     }
 
+    /**
+     * Returns the server object currently running inside Quark Server.
+     *
+     * @return a server that is currently receiving requests from the Internet.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     */
     public static Server server() {
         return server;
     }
 
+    /**
+     * Returns the plugin manager of the Quark Server that handles all the plugins
+     * loaded to Quark Server.
+     *
+     * @return the current plugin manager of Quark Server.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see PluginManager
+     */
     public static PluginManager plugins() {
         return pluginManager;
     }
 
+    /**
+     * Returns the command loop that currently running and reading command
+     * from the console line.
+     *
+     * @return the current command loop of Quark Server.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see CommandLoop
+     */
     public static CommandLoop commandLoop() {
         return commandLoop;
     }
 
+    /**
+     * Returns the command registry that contains all default Quark Server
+     * console commands, and also commands that was registered by loaded
+     * plugins.
+     *
+     * @return the current command registry of Quark Server.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see CommandRegistry
+     */
     public static CommandRegistry commands() {
         return commandRegistry;
     }
 
+    /**
+     * Returns the constructor registry that contains all default Quark Server
+     * constructors, and also constructors that was registered by loaded
+     * plugins.
+     *
+     * @return the current constructor registry of Quark Server.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see EntityConstructorRegistry
+     * @see InstructionEntityConstructor
+     */
     public static EntityConstructorRegistry constructors() {
         return constructorRegistry;
     }
 
+    /**
+     * Returns the instruction registry that contains all default Quark Server
+     * constructors, and also constructors that was registered by loaded
+     * plugins.
+     *
+     * @return the current constructor registry of Quark Server.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see InstructionRegistry
+     * @see Instruction
+     */
     public static InstructionRegistry instructions() {
         return instructionRegistry;
     }
 
+    /**
+     * Runs a single instruction written in Quark QL, waits until its result
+     * and returns the result of instruction execution.
+     * <br><br>
+     *
+     * <strong>Important:</strong> a colon (;) is required at the end of instructions.
+     * <br><br>
+     *
+     * For example, inside the <code>view</code> variable below all the users
+     * which age is greater than 18
+     *
+     * <pre>var view = Quark.runInstruction("select from 'users': if = @condition('age > 18');").tableView();</pre>
+     *
+     * You can read about all the instructions by running the 'list-instructions' command inside your terminal,
+     * or by opening the documentation on official GitHub project: <a href="https://github.com/anafro/quark">here</a>.
+     *
+     * @param  instructionToRun a single instruction that should be executed.
+     * @return the result of instruction execution.
+     * @throws InstructionSyntaxException when instruction passed is malformed.
+     * @throws DatabaseException when the database has crashed on execution,
+     *         or the instruction execution is impossible.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see InstructionRegistry
+     * @see Instruction
+     * @see TableView
+     */
     public static InstructionResult runInstruction(String instructionToRun) {
         server.getInstructionParser().parse(server.getInstructionLexer().lex(instructionToRun));
 
@@ -141,6 +267,32 @@ public final class Quark {
         return instruction.execute(arguments);
     }
 
+    /**
+     * Runs a console command programmatically, and waits until its completion.
+     * The result will be the same as if you run the same command inside your terminal.
+     * <br><br>
+     *
+     * To run the command 'help' from the code, use the following code:
+     *
+     * <pre>Quark.runCommand("help");</pre>
+     *
+     * You can read about all the commands by running the 'help' command inside your terminal,
+     * or by opening the documentation on official GitHub project: <a href="https://github.com/anafro/quark">here</a>.
+     * If you need the help with some command, use the command: help command "&lt;your command name&gt;".
+     *
+     * @param  commandToRun a single instruction that should be executed
+     * @throws CommandSyntaxException when command syntax passed is malformed.
+     * @throws CommandRuntimeException when command execution is impossible, because
+     *         of the arguments you passed or other conditions. You can get more information
+     *         about every situation when CommandRuntimeException can be thrown inside
+     *         all Command class documentations.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see CommandRegistry
+     * @see CommandLoop
+     * @see Command
+     */
     public static void runCommand(String commandToRun) {
         if(commandToRun.isBlank()) {
             return;
@@ -159,6 +311,20 @@ public final class Quark {
         }
     }
 
+    /**
+     * Returns the general logger of Quark Server, which can be used to display
+     * really important messages about the server itself.
+     * <br><br>
+     *
+     * <strong>Please, do not use it to log from modules of your plugin, when knowing the logging source is important!</strong>
+     * Instead, create a Logger class instance inside your class and log from it.
+     *
+     * @return a general logger of Quark Server.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see Logger
+     */
     public static Logger logger() {
         return logger;
     }
