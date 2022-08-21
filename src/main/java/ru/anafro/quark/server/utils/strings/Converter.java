1ee7cc2eae3079f541c732c4490e827a931d1ae5
Анатолий <contact@anafro.ru>
2022-08-21 23:23:44 +1200

    📄 Documented

--- src/main/java/ru/anafro/quark/server/utils/strings/Converter.java
+++ src/main/java/ru/anafro/quark/server/utils/strings/Converter.java
@@ -3,11 +3,39 @@ package ru.anafro.quark.server.utils.strings;
 import ru.anafro.quark.server.utils.exceptions.CallingUtilityConstructorException;
 import ru.anafro.quark.server.utils.strings.exceptions.ObjectFormatException;
 
+/**
+ * A converter can help you to convert strings to all primitive types
+ * you need in one place. We think that Xxx.toXxx() Java syntax is
+ * a bit verbose, so we recommend to use this class instead.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ */
 public final class Converter {
+
+    /**
+     * This private constructor of Converter class <strong>MUST NOT</strong> be ever
+     * called, because Converter is a utility class. Use static methods declared inside.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Converter
+     */
     private Converter() {
         throw new CallingUtilityConstructorException(getClass());
     }
 
+    /**
+     * Converts a string to an integer. If conversation fails, {@link ObjectFormatException}
+     * will be thrown.
+     *
+     * @param string a string that has to be converted to an integer.
+     * @return       the conversation result.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Converter
+     */
     public static int toInteger(String string) {
         try {
             return Integer.parseInt(string);
@@ -16,6 +44,17 @@ public final class Converter {
         }
     }
 
+    /**
+     * Converts a string to a float. If conversation fails, {@link ObjectFormatException}
+     * will be thrown.
+     *
+     * @param string a string that has to be converted to a float.
+     * @return       the conversation result.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Converter
+     */
     public static float toFloat(String string) {
         try {
             return Float.parseFloat(string);
@@ -24,6 +63,17 @@ public final class Converter {
         }
     }
 
+    /**
+     * Converts a string to a long. If conversation fails, {@link ObjectFormatException}
+     * will be thrown.
+     *
+     * @param string a string that has to be converted to a long.
+     * @return       the conversation result.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Converter
+     */
     public static long toLong(String string) {
         try {
             return Long.parseLong(string);
@@ -32,10 +82,23 @@ public final class Converter {
         }
     }
 
+    /**
+     * Converts a string to a boolean. If conversation fails, {@link ObjectFormatException}
+     * will be thrown.
+     *
+     * @param string a string that has to be converted to a boolean.
+     * @return       the conversation result.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Converter
+     */
     public static boolean toBoolean(String string) {
+        // TODO: It seems like this can fail in some cases. Please, read this code carefully to ensure that this code is fine or rewrite it if it doesn't
+
         try {
             return Boolean.parseBoolean(string);
-        } catch(NumberFormatException exception) {
+        } catch(NumberFormatException exception) {  // TODO: Boolean.parseBoolean does not throw NumberFormatException for sure :)
             throw new ObjectFormatException(string, Boolean.class);
         }
     }
