848b3ed902b9f2facb47311d0e90340b23189e0f
Анатолий <contact@anafro.ru>
2022-08-21 23:09:58 +1200

    📄 Documented

--- src/main/java/ru/anafro/quark/server/utils/strings/English.java
+++ src/main/java/ru/anafro/quark/server/utils/strings/English.java
@@ -6,11 +6,44 @@ import ru.anafro.quark.server.utils.exceptions.CallingUtilityConstructorExceptio
 
 import java.util.Objects;
 
+/**
+ * English is a utility class for better looking generated messages.
+ * It can determine an article for a noun, ordinal suffixes for a number and so on.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ */
 public final class English {
+
+    /**
+     * This private constructor of English class <strong>MUST NOT</strong> be ever
+     * called, because English is a utility class. Use static methods declared inside.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    English
+     */
     private English() {
         throw new CallingUtilityConstructorException(getClass());
     }
 
+    /**
+     * Determines an indefinite article for a noun passed to this method.
+     *
+     * <pre>
+     * {@code
+     * English.articleFor("dog");   // a
+     * English.articleFor("apple"); // an
+     * }
+     * </pre>
+     *
+     * @param noun a noun to generate an article for.
+     * @return     a generated article.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    English
+     */
     public static String articleFor(String noun) {
         if(Objects.isNull(noun)) {
             throw new QuarkException("Cannot resolve an article for a null string.");
@@ -27,6 +60,16 @@ public final class English {
         }
     }
 
+    /**
+     * Determines an ordinal suffix for a number (st, nd, rd or th).
+     *
+     * @param number a number to generate an ordinal suffix for.
+     * @return       a determined ordinal suffix.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    English
+     */
     public static String ordinalSuffixFor(int number) {
         return switch(Math.abs(number % 10)) {
             case 1 -> "st";
