d333f460e81fa95410fa362439df3ed9ceac7279
Анатолий <contact@anafro.ru>
2022-08-22 01:40:17 +1200

    📄 Documented

--- src/main/java/ru/anafro/quark/server/security/Token.java
+++ src/main/java/ru/anafro/quark/server/security/Token.java
@@ -1,9 +1,34 @@
 package ru.anafro.quark.server.security;
 
+/**
+ * Tokens are used for separating the access to Quark databases.
+ * Some tokens can do everything, but some can do less - this
+ * is made for security. This is just an interface of looking
+ * up for an existing token and checking its permissions,
+ * not granting them. Use direct queries instead. If token
+ * does not exist in the database, all calls of {@link Token#hasPermission(String)}
+ * will return <code>false</code>.
+ *
+ * @param token a token.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ */
 public record Token(String token) {
-    public boolean hasPermission(String permission) {
-        // TODO
 
-        return false; // TODO
+    /**
+     * Returns <code>true</code> if this token has a permission.
+     * If token does not exist or permission is not granted,
+     * this method will return <code>false</code>.
+     *
+     * @param  permission a checking permission.
+     * @return (see the description)
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     */
+    public boolean hasPermission(String permission) {
+        // TODO: Implement this method
+        return false;
     }
 }
--- src/main/java/ru/anafro/quark/server/types/exceptions/TypeException.java
+++ src/main/java/ru/anafro/quark/server/types/exceptions/TypeException.java
@@ -2,7 +2,24 @@ package ru.anafro.quark.server.types.exceptions;
 
 import ru.anafro.quark.server.exceptions.QuarkException;
 
+/**
+ * This is a super class for all exceptions specific for {@link ru.anafro.quark.server.types}
+ * package. Please, do not throw {@link TypeException} without creating a new class for it.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ */
 public class TypeException extends QuarkException {
+
+    /**
+     * Creates an exception instance. Please, do not throw {@link TypeException}
+     * without creating a new class for it.
+     *
+     * @param message an exception message
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     */
     public TypeException(String message) {
         super(message);
     }
