315299720cb549b9ccbaf6d034bf41211197a14d
Анатолий <contact@anafro.ru>
2022-08-22 01:24:19 +1200

    📄 Documented

--- src/main/java/ru/anafro/quark/server/types/exceptions/WrongByteCountException.java
+++ src/main/java/ru/anafro/quark/server/types/exceptions/WrongByteCountException.java
@@ -1,6 +1,27 @@
 package ru.anafro.quark.server.types.exceptions;
 
+import ru.anafro.quark.server.types.Bytes;
+
+/**
+ * {@link WrongByteCountException} used only inside {@link Bytes} class
+ * when bytes cannot be converted to the desired type.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ * @see    Bytes
+ */
 public class WrongByteCountException extends TypeException {
+
+    /**
+     * Creates an exception instance.
+     *
+     * @param bytesNeeded     bytes count that was needed for a type.
+     * @param actualByteCount bytes count that was received instead.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Bytes
+     */
     public WrongByteCountException(long bytesNeeded, long actualByteCount) {
         super("Byte count is wrong: " + bytesNeeded + " needed, but there are " + actualByteCount);
     }
