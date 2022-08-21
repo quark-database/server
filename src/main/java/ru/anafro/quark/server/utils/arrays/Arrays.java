efa4c33f751aa96358e2c19925808933ef63f387
Анатолий <contact@anafro.ru>
2022-08-22 01:21:36 +1200

    📄 Documented

--- src/main/java/ru/anafro/quark/server/utils/arrays/Arrays.java
+++ src/main/java/ru/anafro/quark/server/utils/arrays/Arrays.java
@@ -4,17 +4,56 @@ import ru.anafro.quark.server.utils.exceptions.CallingUtilityConstructorExceptio
 
 import java.util.Random;
 
+/**
+ * Arrays class contains uncategorized utility methods for
+ * arrays manipulating. See static methods descriptions to learn more.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ * @see    Arrays#random(Object[])
+ */
 public final class Arrays {
     private static final Random random = new Random();
 
+    /**
+     * This private constructor of Arrays class <strong>MUST NOT</strong> be ever
+     * called, because Arrays is a utility class. Use static methods declared inside.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Arrays
+     */
     private Arrays() {
         throw new CallingUtilityConstructorException(getClass());
     }
 
-    public static <T> T random(T[] array) {
+    /**
+     * Returns a random element from the array.
+     *
+     * @param     array an array where a random element will be picked from.
+     * @return    a random element from this array.
+     * @param <T> a type of array.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Arrays
+     */
+    public static <T> T random(T[] array) { // TODO: It seems like this can fail in some cases. Please, read this code carefully to ensure that this code is fine or rewrite it if it doesn't
         return array[random.nextInt(array.length)];
     }
 
+    /**
+     * Returns <code>true</code> if <code>array</code> contains <code>value</code>.
+     * Otherwise, <code>false</code> is returned.
+     *
+     * @param array an array where this method will be searching for the value.
+     * @param value a value which will be searched in the array.
+     * @return      (see the description)
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Arrays
+     */
     public static boolean contains(Object[] array, Object value) {
         for(Object element : array) {
             if(element.equals(value)) {
@@ -25,6 +64,17 @@ public final class Arrays {
         return false;
     }
 
+    /**
+     * Creates an array of objects passed to this method.
+     *
+     * @param objects objects that will be in an array.
+     * @return        an array of objects passed.
+     * @param <T>     a type of objects.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Arrays
+     */
     @SafeVarargs
     public static <T> T[] of(T... objects) {
         return objects;
--- src/main/java/ru/anafro/quark/server/utils/containers/Lists.java
+++ src/main/java/ru/anafro/quark/server/utils/containers/Lists.java
@@ -5,11 +5,49 @@ import ru.anafro.quark.server.utils.strings.TextBuffer;
 
 import java.util.List;
 
+/**
+ * Lists class contains uncategorized utility methods for
+ * list manipulating. See static methods descriptions to learn more.
+ *
+ * @since  Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ * @see    Lists#join(List)
+ */
 public final class Lists {
+
+    /**
+     * This private constructor of Lists class <strong>MUST NOT</strong> be ever
+     * called, because Lists is a utility class. Use static methods declared inside.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Lists
+     */
     private Lists() {
         throw new CallingUtilityConstructorException(getClass());
     }
 
+    /**
+     * Joins a list to a string with a separator between each element.
+     *
+     * <pre>
+     * {@code
+     * var entities = List.of("anafro", "java", "norway", "dogs");
+     *
+     * // anafro + java + norway + dogs = love
+     * Lists.join(entities, " + ") + " = love";
+     * }
+     * </pre>
+     *
+     * @param collection a list to join.
+     * @param separator  a separator that will be put between each element, <i>but not at the end of the string</i>.
+     * @return           a list joined with a separator between each element.
+     * @param <T>        a type of list.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Lists
+     */
     public static <T> String join(List<? extends T> collection, String separator) {
         TextBuffer joinedContainer = new TextBuffer();
 
@@ -24,6 +62,17 @@ public final class Lists {
         return joinedContainer.extractContent();
     }
 
+    /**
+     * Joins a list with a comma as a separator between each element.
+     *
+     * @param collection a list to join.
+     * @return           a list joined with a comma between each element.
+     * @param <T>        a list type.
+     *
+     * @since  Quark 1.1
+     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @see    Lists
+     */
     public static <T> String join(List<? extends T> collection) {
         return join(collection, ", ");
     }
