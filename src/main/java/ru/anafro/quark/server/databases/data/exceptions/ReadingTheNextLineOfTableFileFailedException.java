53d82f29e8f1db6c91dd280efa558f83eee09147
Анатолий <contact@anafro.ru>
2022-08-23 23:32:25 +1200

    🖋️ Renamed (TableFile -> RecordsFile)

--- src/main/java/ru/anafro/quark/server/databases/data/exceptions/ReadingTheNextLineOfTableFileFailedException.java
+++ src/main/java/ru/anafro/quark/server/databases/data/exceptions/ReadingTheNextLineOfTableFileFailedException.java
@@ -1,12 +1,12 @@
 package ru.anafro.quark.server.databases.data.exceptions;
 
-import ru.anafro.quark.server.databases.data.TableFile;
+import ru.anafro.quark.server.databases.data.files.RecordsFile;
 
 import static ru.anafro.quark.server.utils.strings.Wrapper.quoted;
 
 public class ReadingTheNextLineOfTableFileFailedException extends DatabaseFileException {
-    public ReadingTheNextLineOfTableFileFailedException(TableFile tableFile, Throwable cause) {
-        super("Reading the next line of the table file %s failed, because of %s".formatted(quoted(tableFile.getFilename()), cause.toString()));
+    public ReadingTheNextLineOfTableFileFailedException(RecordsFile recordsFile, Throwable cause) {
+        super("Reading the next line of the table file %s failed, because of %s".formatted(quoted(recordsFile.getFilename()), cause.toString()));
         initCause(cause);
     }
 }
