7bcdeeca51af787ddf4cd8279bd1dcc9aea2806c
Анатолий <contact@anafro.ru>
2022-08-23 23:35:51 +1200

    🖋️ Renamed (TableFile -> RecordsFile)

--- src/main/java/ru/anafro/quark/server/databases/data/exceptions/DatabaseFileNotFoundException.java
+++ src/main/java/ru/anafro/quark/server/databases/data/exceptions/DatabaseFileNotFoundException.java
@@ -1,11 +1,11 @@
 package ru.anafro.quark.server.databases.data.exceptions;
 
-import ru.anafro.quark.server.databases.data.TableFile;
+import ru.anafro.quark.server.databases.data.files.RecordsFile;
 
 import static ru.anafro.quark.server.utils.strings.Wrapper.quoted;
 
 public class DatabaseFileNotFoundException extends DatabaseFileException {
-    public DatabaseFileNotFoundException(TableFile tableFile) {
-        super("The table file %s does not exist.".formatted(quoted(tableFile.getFilename())));
+    public DatabaseFileNotFoundException(RecordsFile recordsFile) {
+        super("The table file %s does not exist.".formatted(quoted(recordsFile.getFilename())));
     }
 }
