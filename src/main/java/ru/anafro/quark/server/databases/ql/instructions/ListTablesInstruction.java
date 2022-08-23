a9ee6b7df4e9df494c37dcc4dde31667bab1d71b
Анатолий <contact@anafro.ru>
2022-08-23 23:28:47 +1200

    ✨ Added "list tables in" instruction functionality

--- src/main/java/ru/anafro/quark/server/databases/ql/instructions/ListTablesInstruction.java
+++ src/main/java/ru/anafro/quark/server/databases/ql/instructions/ListTablesInstruction.java
@@ -1,9 +1,10 @@
 package ru.anafro.quark.server.databases.ql.instructions;
 
-import ru.anafro.quark.server.databases.ql.Instruction;
-import ru.anafro.quark.server.databases.ql.InstructionArguments;
-import ru.anafro.quark.server.databases.ql.InstructionParameter;
-import ru.anafro.quark.server.databases.ql.InstructionResultRecorder;
+import ru.anafro.quark.server.databases.data.Database;
+import ru.anafro.quark.server.databases.ql.*;
+import ru.anafro.quark.server.databases.ql.entities.StringEntity;
+import ru.anafro.quark.server.databases.views.TableViewHeader;
+import ru.anafro.quark.server.databases.views.TableViewRow;
 import ru.anafro.quark.server.networking.Server;
 
 public class ListTablesInstruction extends Instruction {
@@ -16,6 +17,12 @@ public class ListTablesInstruction extends Instruction {
 
     @Override
     public void action(InstructionArguments arguments, Server server, InstructionResultRecorder result) {
+        result.header(new TableViewHeader("table name"));
 
+        for(var table : Database.byName(arguments.<StringEntity>get("database").getValue()).allTables()) {
+            result.appendRow(new TableViewRow(table.getName()));
+        }
+
+        result.status(InstructionExecutionStatus.OK, "Table names are loaded.");
     }
 }
