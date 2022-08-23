86795f83aed07927478bf1008655786041d24f87
Анатолий <contact@anafro.ru>
2022-08-23 23:27:40 +1200

    ✨ Added "list databases" instruction functionality

--- src/main/java/ru/anafro/quark/server/databases/ql/instructions/ListDatabasesInstruction.java
+++ src/main/java/ru/anafro/quark/server/databases/ql/instructions/ListDatabasesInstruction.java
@@ -1,8 +1,12 @@
 package ru.anafro.quark.server.databases.ql.instructions;
 
+import ru.anafro.quark.server.databases.data.Database;
 import ru.anafro.quark.server.databases.ql.Instruction;
 import ru.anafro.quark.server.databases.ql.InstructionArguments;
+import ru.anafro.quark.server.databases.ql.InstructionExecutionStatus;
 import ru.anafro.quark.server.databases.ql.InstructionResultRecorder;
+import ru.anafro.quark.server.databases.views.TableViewHeader;
+import ru.anafro.quark.server.databases.views.TableViewRow;
 import ru.anafro.quark.server.networking.Server;
 
 public class ListDatabasesInstruction extends Instruction {
@@ -12,6 +16,12 @@ public class ListDatabasesInstruction extends Instruction {
 
     @Override
     public void action(InstructionArguments arguments, Server server, InstructionResultRecorder result) {
+        result.header(new TableViewHeader("database name"));
 
+        for(var database : Database.all()) {
+            result.appendRow(new TableViewRow(database.getName()));
+        }
+
+        result.status(InstructionExecutionStatus.OK, "Database names are loaded.");
     }
 }
