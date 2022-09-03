4d5b66e357187954d7dc69dce104a144eb3fdaaa
Анатолий <contact@anafro.ru>
2022-09-04 11:44:19 +1200

    ✨ Moved all the functionality from object recognizer to here

--- src/main/java/ru/anafro/quark/server/databases/ql/lexer/states/ReadingObjectInstructionLexerState.java
+++ src/main/java/ru/anafro/quark/server/databases/ql/lexer/states/ReadingObjectInstructionLexerState.java
@@ -1,7 +1,10 @@
 package ru.anafro.quark.server.databases.ql.lexer.states;
 
+import ru.anafro.quark.server.databases.ql.exceptions.InstructionSyntaxException;
 import ru.anafro.quark.server.databases.ql.lexer.InstructionLexer;
-import ru.anafro.quark.server.databases.ql.lexer.states.helpers.InstructionObjectRecognizer;
+import ru.anafro.quark.server.databases.ql.lexer.tokens.ConstructorNameInstructionToken;
+import ru.anafro.quark.server.databases.ql.lexer.tokens.StringLiteralInstructionToken;
+import ru.anafro.quark.server.utils.arrays.Arrays;
 
 public class ReadingObjectInstructionLexerState extends InstructionLexerState {
     public ReadingObjectInstructionLexerState(InstructionLexer lexer, InstructionLexerState previousState) {
@@ -11,9 +14,20 @@ public class ReadingObjectInstructionLexerState extends InstructionLexerState {
 
     @Override
     public void handleCharacter(char currentCharacter) {
-        logger.debug("Expecting an object, so let's an instruction recognizer deal with it");
+        logger.debug("Expecting an object, trying to recognise an object type");
         stopSkippingLexerIgnoredCharacters();
+
         lexer.letTheNextStateStartFromCurrentCharacter();
-        lexer.switchState(new InstructionObjectRecognizer().recognizeObjectAndMakeLexerState(lexer, getPreviousState()));
+
+        if(currentCharacter == ConstructorNameInstructionToken.CONSTRUCTOR_NAME_MARKER) {
+            lexer.switchState(new ReadingConstructorNameInstructionLexerState(lexer, getPreviousState()));
+        } else if(currentCharacter == StringLiteralInstructionToken.STRING_LITERAL_QUOTE) {
+            lexer.switchState(new ReadingStringInstructionLexerState(lexer, getPreviousState()));
+        } else if(Character.isDigit(currentCharacter) || Arrays.contains(new Character[] {'+', '-'}, currentCharacter)) {
+            lexer.switchState(new ReadingNumberInstructionLexerState(lexer, getPreviousState()));
+        } else {
+            // TODO: Replace '@' with an existing constant
+            throw new InstructionSyntaxException(getPreviousState(), lexer.getInstruction(), "Object expected, but none of the values can be started with '" + currentCharacter + "'", "Did you make a typo? Or missed '@' before constructor name? E.g. @list(). Note that constants must also start with that symbol.", lexer.getCurrentCharacterIndex(), 1);
+        }
     }
 }
