8e1e6b2e5dc99ca93ce2182ef7a7570bf30d9079
Анатолий <contact@anafro.ru>
2022-09-04 22:19:06 +1200

    📥 Registered a new `modifiers` command

--- src/main/java/ru/anafro/quark/server/api/Quark.java
+++ src/main/java/ru/anafro/quark/server/api/Quark.java
@@ -256,6 +256,7 @@ public final class Quark {
         commandRegistry.add(new RunCommand());
         commandRegistry.add(new TestCommand());
         commandRegistry.add(new EvalCommand());
+        commandRegistry.add(new ModifiersCommand());
 //        commandRegistry.add(new ReloadCommand());        TODO: Not working yet
 
         pluginManager.loadPlugins();
