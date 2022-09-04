dff13c2e4a711e6b39dda6daedc846f9c81ace89
Анатолий <contact@anafro.ru>
2022-09-04 22:13:48 +1200

    🔥 Added a compounded table name support

--- src/main/java/ru/anafro/quark/server/databases/data/Table.java
+++ src/main/java/ru/anafro/quark/server/databases/data/Table.java
@@ -1,22 +1,37 @@
 package ru.anafro.quark.server.databases.data;
 
+import ru.anafro.quark.server.api.Quark;
+import ru.anafro.quark.server.databases.data.exceptions.WrongCompoundedTableNameException;
 import ru.anafro.quark.server.databases.data.files.HeaderFile;
 import ru.anafro.quark.server.databases.data.files.RecordsFile;
 import ru.anafro.quark.server.databases.data.files.VariableFolder;
+import ru.anafro.quark.server.databases.views.TableViewHeader;
 import ru.anafro.quark.server.utils.containers.Lists;
+import ru.anafro.quark.server.utils.objects.Nulls;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class Table {
+    public static final String COMPOUNDED_TABLE_NAME_SEPARATOR = ".";
     private final String name;
     private final Database database;
     private final HeaderFile headerFile;
     private final RecordsFile recordsFile;
     private final VariableFolder variableFolder;
 
-    public Table(String name, Database database) {
-        this.name = name;
-        this.database = database;
+    public static Table byName(String compoundedName) {
+        if(!compoundedName.contains(COMPOUNDED_TABLE_NAME_SEPARATOR)) {
+            throw new WrongCompoundedTableNameException(compoundedName);
+        }
+
+        var compoundedNameParts = compoundedName.split(Pattern.quote(COMPOUNDED_TABLE_NAME_SEPARATOR), 2);
+        return new Table(compoundedNameParts[0], compoundedNameParts[1]);
+    }
+
+    public Table(String databaseName, String tableName) {
+        this.name = tableName;
+        this.database = Database.byName(databaseName);
         this.headerFile = new HeaderFile(this);
         this.recordsFile = new RecordsFile(this);
         this.variableFolder = new VariableFolder(this);
@@ -58,7 +73,10 @@ public class Table {
         final long[] recordsLeftToSkip = { skip };
         final long[] recordsLeftToSelect = { limit };
 
-        recordsFile.forEach(record -> {
+
+        for(var record : recordsFile) {
+            Quark.logger().error(Nulls.evalOrDefault(record, TableRecord::toTableLine, "<null table record>"));
+
             if(selector.shouldBeSelected(record)) {
                 if(recordsLeftToSelect[0] > 0) {
                     if(recordsLeftToSkip[0] > 0) {
@@ -69,7 +87,7 @@ public class Table {
                     }
                 }
             }
-        });
+        }
 
         return selectedRecords;
     }
@@ -80,5 +98,9 @@ public class Table {
                 // TODO change the record?
             }
         });
+    }
+
+    public TableViewHeader createTableViewHeader() {
+        return new TableViewHeader(headerFile.getColumns().stream().map(ColumnDescription::getName).toArray(String[]::new));
     }
 }
