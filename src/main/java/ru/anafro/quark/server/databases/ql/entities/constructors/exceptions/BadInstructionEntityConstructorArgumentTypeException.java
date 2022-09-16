d05e88ce11f597007706cb039df55258b6bff656
Анатолий <contact@anafro.ru>
2022-09-17 02:51:32 +1200

    🖋️ Renamed `InstructionEntityConstructor` to `EntityConstructor`

--- src/main/java/ru/anafro/quark/server/databases/ql/entities/constructors/exceptions/BadInstructionEntityConstructorArgumentTypeException.java
+++ src/main/java/ru/anafro/quark/server/databases/ql/entities/constructors/exceptions/BadInstructionEntityConstructorArgumentTypeException.java
@@ -1,10 +1,10 @@
 package ru.anafro.quark.server.databases.ql.entities.constructors.exceptions;
 
 import ru.anafro.quark.server.databases.exceptions.DatabaseException;
-import ru.anafro.quark.server.databases.ql.entities.InstructionEntityConstructor;
+import ru.anafro.quark.server.databases.ql.entities.EntityConstructor;
 
 public class BadInstructionEntityConstructorArgumentTypeException extends DatabaseException { // This should be viewed in code editor, change to InstructionSyntaxException
-    public BadInstructionEntityConstructorArgumentTypeException(InstructionEntityConstructor constructor, String parameterName) {
+    public BadInstructionEntityConstructorArgumentTypeException(EntityConstructor constructor, String parameterName) {
         super("%s's value passed to constructor %s is wrong, because it should have type %s. Check out this constructor syntax: %s".formatted(parameterName, constructor.getName(), constructor.getParameters().getParameter(parameterName).type(), constructor.getSyntax()));
     }
 }
