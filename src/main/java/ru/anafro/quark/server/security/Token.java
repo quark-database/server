ced4702e136b1460bda33d48e01a1741ea0a6cc2
Анатолий <contact@anafro.ru>
2022-10-06 17:13:54 +1000

    🔥 Added `generate()`

--- src/main/java/ru/anafro/quark/server/security/Token.java
+++ src/main/java/ru/anafro/quark/server/security/Token.java
@@ -1,6 +1,8 @@
 package ru.anafro.quark.server.security;
 
 import ru.anafro.quark.server.api.Quark;
+import ru.anafro.quark.server.utils.arrays.Arrays;
+import ru.anafro.quark.server.utils.strings.TextBuffer;
 
 /**
  * Tokens are used for separating the access to Quark databases.
@@ -18,6 +20,19 @@ import ru.anafro.quark.server.api.Quark;
  * @author  Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
  */
 public record Token(String token) {
+
+    private static final String GENERATED_TOKENS_CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyz-_ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int GENERATED_TOKENS_LENGTH = 256;
+
+    public static String generate() {
+        var token = new TextBuffer();
+
+        for(int index = 0; index < GENERATED_TOKENS_LENGTH; index++) {
+            token.append(Arrays.<Character>random(GENERATED_TOKENS_CHARACTERS.chars().mapToObj(c -> (char) c).toArray(Character[]::new)));
+        }
+
+        return token.extractContent();
+    }
 
     /**
      * Returns {@code true} if this token has a permission.
