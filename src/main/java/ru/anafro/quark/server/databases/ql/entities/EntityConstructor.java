5a0d21dbf6c18283fc53d73a11bd4f65d95c6985
Анатолий <contact@anafro.ru>
2022-10-06 17:57:11 +1000

    🔥 Improved the `getSyntax()` output

--- src/main/java/ru/anafro/quark/server/databases/ql/entities/EntityConstructor.java
+++ src/main/java/ru/anafro/quark/server/databases/ql/entities/EntityConstructor.java
@@ -3,19 +3,20 @@ package ru.anafro.quark.server.databases.ql.entities;
 import ru.anafro.quark.server.api.Quark;
 import ru.anafro.quark.server.databases.ql.entities.exceptions.ConstructorEvaluationException;
 import ru.anafro.quark.server.databases.ql.lexer.tokens.ConstructorNameInstructionToken;
+import ru.anafro.quark.server.exceptions.QuarkException;
 import ru.anafro.quark.server.utils.containers.Lists;
 import ru.anafro.quark.server.utils.strings.TextBuffer;
-
-import java.util.stream.Collectors;
 
 import static ru.anafro.quark.server.utils.strings.Wrapper.quoted;
 
 public abstract class EntityConstructor {
     private final String name;
     private final InstructionEntityConstructorParameters parameters;
+    private final InstructionEntityConstructorReturnDescription returnDescription;
 
-    public EntityConstructor(String name, InstructionEntityConstructorParameter... parameters) {
+    public EntityConstructor(String name, InstructionEntityConstructorReturnDescription returnDescription, InstructionEntityConstructorParameter... parameters) {
         this.name = name;
+        this.returnDescription = returnDescription;
         this.parameters = new InstructionEntityConstructorParameters(parameters);
     }
 
@@ -60,7 +61,30 @@ public abstract class EntityConstructor {
             }
         }
 
-        return invoke(arguments);
+        var result = invoke(arguments);
+
+        if(result == null) {
+            throw new QuarkException("Result of " + ConstructorNameInstructionToken.CONSTRUCTOR_NAME_MARKER + name + " is null. Provided arguments: " + Lists.join(arguments.getArgumentsAsList().stream().map(argument -> argument.getName() + " = " + argument.getEntity().toInstructionForm()).toList()));
+        }
+
+        if(result.mismatchesType(returnDescription.getType())) {
+            if(returnDescription.getType().castableFrom(result.getType())) {
+                result = returnDescription.getType().cast(result);
+            } else {
+                throw new ConstructorEvaluationException(this, "Constructor %s must return an entity with type %s or an entity that can be casted to this type, but it returned an entity %s with type %s.".formatted(
+                        name,
+                        returnDescription.getType().getName(),
+                        result.toInstructionForm(),
+                        result.getExactTypeName()
+                ));
+            }
+        }
+
+        return result;
+    }
+
+    public InstructionEntityConstructorReturnDescription getReturnDescription() {
+        return returnDescription;
     }
 
     public String getSyntax() {
@@ -69,10 +93,27 @@ public abstract class EntityConstructor {
         syntax.append(ConstructorNameInstructionToken.CONSTRUCTOR_NAME_MARKER);
         syntax.append(name);
         syntax.append('(');
+        syntax.nextLine();
 
-        syntax.append(Lists.join(parameters.asList().stream().map(parameter -> parameter.name() + " is " + (parameter.required() ? "" : "optional " ) + parameter.type() + (parameter.isVarargs() ? InstructionEntityConstructorParameter.VARARGS_TYPE_MARKER : "")).collect(Collectors.toList()), ", "));
+        for(int index = 0; index < parameters.asList().size(); index++) {
+            var parameter = parameters.parameterAt(index);
+            syntax.append("\t");
+
+            syntax.append(parameter.name() + " is " + (parameter.required() ? "" : "optional " ) + parameter.type() + (parameter.isVarargs() ? InstructionEntityConstructorParameter.VARARGS_TYPE_MARKER : ""));
+
+            if(index + 1 != parameters.asList().size()) {
+                syntax.append(',');
+            }
+
+            syntax.nextLine();
+        }
 
         syntax.append(')');
+
+        syntax.append(" -> ");
+        syntax.append(returnDescription.getDescription());
+        syntax.append(" is ");
+        syntax.append(returnDescription.getType().getName());
 
         return syntax.extractContent();
     }
