4b16ba43c3aaf35b1ec06f5647b6ab8b99a5cf07
Анатолий <contact@anafro.ru>
2022-10-06 17:39:59 +1000

    🔥 Abstracted

--- src/main/java/ru/anafro/quark/server/databases/data/TableRecordSelector.java
+++ src/main/java/ru/anafro/quark/server/databases/data/TableRecordSelector.java
@@ -1,29 +1,17 @@
 package ru.anafro.quark.server.databases.data;
 
-import ru.anafro.quark.server.api.Quark;
-import ru.anafro.quark.server.databases.data.exceptions.TableRecordSelectorExpressionShouldBeBooleanException;
-import ru.anafro.quark.server.databases.ql.ConstructorEvaluator;
-import ru.anafro.quark.server.databases.ql.entities.BooleanEntity;
-import ru.anafro.quark.server.databases.ql.entities.constructors.StringConstructorBuilder;
+public abstract class TableRecordSelector implements RecordLambda<Boolean> {
+    private final String name;
 
-public record TableRecordSelector(String expression) {
-    public static final String COLUMN_NAME_MARKER = ":";
+    public TableRecordSelector(String name) {
+        this.name = name;
+    }
 
-    public static final TableRecordSelector SELECT_ALL = new TableRecordSelector(new StringConstructorBuilder().name("yes").build());
+    public String getName() {
+        return name;
+    }
 
-    public boolean shouldBeSelected(TableRecord record) {
-        String filledExpression = expression;
-
-        for(var field : record) {
-            filledExpression = filledExpression.replace(COLUMN_NAME_MARKER + field.getColumnName(), field.getValue().toInstructionForm());
-        }
-
-        var result = ConstructorEvaluator.eval(filledExpression);
-
-        if(result.mismatchesType(Quark.types().get("boolean"))) {
-            throw new TableRecordSelectorExpressionShouldBeBooleanException(expression, result.getType());
-        }
-
-        return ((BooleanEntity) result).getValue();
+    public boolean is(String name) {
+        return name.equals(this.name);
     }
 }
