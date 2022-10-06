7fdc5f134dcc7ede761eb715b93cef5827c5e404
Анатолий <contact@anafro.ru>
2022-10-06 17:15:25 +1000

    🔥 Supported console colors

--- src/main/java/ru/anafro/quark/server/logging/Logger.java
+++ src/main/java/ru/anafro/quark/server/logging/Logger.java
@@ -1,23 +1,52 @@
 package ru.anafro.quark.server.logging;
 
+import ru.anafro.quark.server.utils.Console;
+import ru.anafro.quark.server.utils.arrays.Arrays;
+import ru.anafro.quark.server.utils.debug.CallStack;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Logger {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     private final String label;
     private String format = "@level @time, @label | @message";
     private LogLevel logFrom = LogLevel.INFO;
 
-    public Logger(String label) {
+    public Logger(String label, LogLevel logFrom) {
         this.label = label;
+        this.logFrom = logFrom;
+    }
+
+    public Logger(LogLevel logFrom) {
+        this("Unnamed Logger");
+        this.logFrom = logFrom;
+    }
+
+    public Logger(Class<?> clazz, LogLevel logFrom) {
+        this(clazz.getSimpleName());
+        this.logFrom = logFrom;
+    }
+
+    public Logger(String label) {
+        this(label, LogLevel.INFO);
     }
 
     public Logger() {
-        this("Default Logger");
+        this(LogLevel.INFO);
     }
 
     public Logger(Class<?> clazz) {
-        this(clazz.getSimpleName());
+        this(clazz, LogLevel.INFO);
     }
 
     public String getLabel() {
@@ -33,13 +62,19 @@ public class Logger {
     }
 
     public void log(LogLevel logLevel, String message) {
+        var lines = message == null ? Arrays.of("<null message>") : message.split("\n");
+
         if(logLevel.ordinal() >= logFrom.ordinal()) {
-            System.out.println(format
-                    .replace("@level", logLevel.name())
-                    .replace("@time", new SimpleDateFormat("HH:mm").format(new Date()))
-                    .replace("@label", label)
-                    .replace("@message", message == null ? "<null message>" : message)
-            );
+            for(var line : lines) {
+                Console.synchronizedPrintln(
+                        (logLevel == LogLevel.DEBUG ? ANSI_PURPLE + "Logged from: " + CallStack.ofDepth(4) + ANSI_RESET + "\t" : "") +
+                        format
+                            .replace("@level", logLevel.getColor() + logLevel.name() + ANSI_RESET)
+                            .replace("@time", new SimpleDateFormat("HH:mm").format(new Date()))
+                            .replace("@label", label)
+                            .replace("@message", line)
+                );
+            }
         }
     }
 
