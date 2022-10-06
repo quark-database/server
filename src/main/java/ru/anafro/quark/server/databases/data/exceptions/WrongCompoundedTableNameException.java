bc3311cc76dbfd83696b084c1c585180194e5277
Анатолий <contact@anafro.ru>
2022-10-06 17:42:31 +1000

    🔥 Changed compounded name source

--- src/main/java/ru/anafro/quark/server/databases/data/exceptions/WrongCompoundedTableNameException.java
+++ src/main/java/ru/anafro/quark/server/databases/data/exceptions/WrongCompoundedTableNameException.java
@@ -1,13 +1,13 @@
 package ru.anafro.quark.server.databases.data.exceptions;
 
-import ru.anafro.quark.server.databases.data.Table;
+import ru.anafro.quark.server.databases.data.CompoundedTableName;
 
 public class WrongCompoundedTableNameException extends DatabaseFileException {
     public WrongCompoundedTableNameException(String wrongCompoundedName) {
         super("The compounded name '%s' is wrong, because it must contain one symbol '%s', which separates the database name and table name. Use this form: DatabaseName%sTableName".formatted(
                 wrongCompoundedName,
-                Table.COMPOUNDED_TABLE_NAME_SEPARATOR,
-                Table.COMPOUNDED_TABLE_NAME_SEPARATOR
+                CompoundedTableName.SEPARATOR,
+                CompoundedTableName.SEPARATOR
         ));
     }
 }
