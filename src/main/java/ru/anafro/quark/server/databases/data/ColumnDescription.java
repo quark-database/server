40a938072a908582d4978d2ba1443cb70e5afbfa
Анатолий <contact@anafro.ru>
2022-10-06 17:35:57 +1000

    🔥 Pulled `ColumnModifier` to `ColumnModifierEntity`

--- src/main/java/ru/anafro/quark/server/databases/data/ColumnDescription.java
+++ src/main/java/ru/anafro/quark/server/databases/data/ColumnDescription.java
@@ -1,5 +1,6 @@
 package ru.anafro.quark.server.databases.data;
 
+import ru.anafro.quark.server.databases.ql.entities.ColumnModifierEntity;
 import ru.anafro.quark.server.databases.ql.types.EntityType;
 
 import java.util.ArrayList;
@@ -7,9 +8,9 @@ import java.util.ArrayList;
 public class ColumnDescription {
     private final String name;
     private final EntityType type;
-    private final ArrayList<ColumnModifier> modifiers;
+    private final ArrayList<ColumnModifierEntity> modifiers;
 
-    public ColumnDescription(String name, EntityType type, ArrayList<ColumnModifier> modifiers) {
+    public ColumnDescription(String name, EntityType type, ArrayList<ColumnModifierEntity> modifiers) {
         this.name = name;
         this.type = type;
         this.modifiers = modifiers;
@@ -23,7 +24,7 @@ public class ColumnDescription {
         return type;
     }
 
-    public ArrayList<ColumnModifier> getModifiers() {
+    public ArrayList<ColumnModifierEntity> getModifiers() {
         return modifiers;
     }
 }
