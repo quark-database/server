0b93b74c07745a00e2dd6eeeaee012f9bd905a49
Анатолий <contact@anafro.ru>
2022-10-06 17:06:10 +1000

    🐶 Just created!

---
+++ src/main/java/ru/anafro/quark/server/databases/ql/entities/constructors/HoursConstructor.java
@@ -0,0 +1,24 @@
+package ru.anafro.quark.server.databases.ql.entities.constructors;
+
+import ru.anafro.quark.server.databases.ql.entities.*;
+
+import static ru.anafro.quark.server.databases.ql.entities.InstructionEntityConstructorParameter.required;
+import static ru.anafro.quark.server.databases.ql.entities.InstructionEntityConstructorReturnDescription.returns;
+
+public class HoursConstructor extends EntityConstructor {
+
+    public HoursConstructor() {
+        super(
+                "hours",
+
+                returns("the amount of milliseconds", "long"),
+
+                required("hours", "float")
+        );
+    }
+
+    @Override
+    protected Entity invoke(InstructionEntityConstructorArguments arguments) {
+        return new FloatEntity(arguments.getFloat("seconds") * 360_000.0F);
+    }
+}
