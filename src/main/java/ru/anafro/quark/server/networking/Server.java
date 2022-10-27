2ce19ebfb72740b42630ff298abc36f712a3df92
Анатолий <contact@anafro.ru>
2022-10-27 17:41:25 +1000

    🔥 Fired events

--- src/main/java/ru/anafro/quark/server/networking/Server.java
+++ src/main/java/ru/anafro/quark/server/networking/Server.java
@@ -2,15 +2,17 @@ package ru.anafro.quark.server.networking;
 
 import ru.anafro.quark.server.api.Quark;
 import ru.anafro.quark.server.databases.ql.InstructionResult;
+import ru.anafro.quark.server.databases.ql.QueryExecutionStatus;
 import ru.anafro.quark.server.databases.ql.lexer.InstructionLexer;
 import ru.anafro.quark.server.databases.ql.parser.InstructionParser;
+import ru.anafro.quark.server.databases.views.TableView;
+import ru.anafro.quark.server.exceptions.QuarkException;
 import ru.anafro.quark.server.logging.Logger;
-import ru.anafro.quark.server.plugins.Plugin;
-import ru.anafro.quark.server.plugins.events.ServerStarted;
+import ru.anafro.quark.server.plugins.events.*;
 import ru.anafro.quark.server.security.Token;
 
 public class Server extends TcpServer {
-    private final ServerConfiguration configuration = new ServerConfigurationLoader().load("Server Configuration.yaml");
+    private final ServerConfiguration configuration = new ServerConfigurationLoader().load("Server Configuration.json");
     private final InstructionLexer lexer = new InstructionLexer();
     private final InstructionParser parser = new InstructionParser();
 
@@ -32,33 +34,72 @@ public class Server extends TcpServer {
     public void start() {
         logger.info("Server is being started at port " + configuration.getPort() + "...");
 
+        Quark.plugins().forEach(plugin -> {
+            try {
+                plugin.onEnable();
+                Quark.fire(new PluginLoaded(plugin));
+            } catch(Throwable throwable) {
+                Quark.fire(new PluginCrashed(plugin, throwable));
+            }
+        });
+
+        Quark.fire(new BeforeServerStarted(this));
         super.start(configuration.getPort());
-        Quark.plugins().getLoadedPlugins().forEach(Plugin::onEnable);
+        Quark.fire(new ServerStarted(this));
     }
 
     @Override
     public Response onRequest(Request request) {
-        parser.parse(lexer.lex(request.getString("query")));
-        var instruction = parser.getInstruction();
-        var arguments = parser.getArguments();
-        var token = new Token(request.getString("token"));
+        try {
+            var query = request.getString("query");
 
-        if(token.hasPermission(instruction.getPermission())) {
-            InstructionResult result = instruction.execute(arguments);
+            Quark.fire(new RequestReceived(this, request));
+            Quark.fire(new BeforeInstructionLexing(query, lexer));
 
+            var tokens = lexer.lex(query);
+
+            Quark.fire(new BeforeInstructionParsing(query, tokens, parser));
+            parser.parse(tokens);
+
+            var instruction = parser.getInstruction();
+            var arguments = parser.getArguments();
+            var token = new Token(request.getString("token"));
+
+
+            if(token.hasPermission(instruction.getPermission())) {
+                    Quark.fire(new BeforeInstructionExecuted(instruction, arguments));
+
+                    InstructionResult result = instruction.execute(arguments);
+
+                    Quark.fire(new InstructionFinished(instruction, arguments, result));
+
+                    return Response.create()
+                            .add("status", result.queryExecutionStatus().name())
+                            .add("message", result.message())
+                            .add("time", result.milliseconds())
+                            .add("table", result.tableView().toJson());
+
+            } else {
+                return Response.error("No Permission");
+            }
+        } catch(QuarkException exception) {
             return Response.create()
-                    .add("status", result.queryExecutionStatus().name())
-                    .add("message", result.message())
-                    .add("time", result.milliseconds())
-                    .add("table", result.tableView().toJson());
-        } else {
-            return Response.error("No Permission");
+                    .add("status", QueryExecutionStatus.SYNTAX_ERROR.name())
+                    .add("message", exception.getMessage())
+                    .add("time", 0)
+                    .add("table", TableView.empty().toJson());
+        } catch(Exception exception) {
+            return Response.create()
+                    .add("status", QueryExecutionStatus.SERVER_ERROR.name())
+                    .add("message", exception.getMessage())
+                    .add("time", 0)
+                    .add("table", TableView.empty().toJson());
         }
     }
 
     @Override
     public void onStartingCompleted() {
-        Quark.plugins().fireEvent(new ServerStarted());
+        Quark.fire(new ServerStarted(this));
     }
 
     public ServerConfiguration getConfiguration() {
