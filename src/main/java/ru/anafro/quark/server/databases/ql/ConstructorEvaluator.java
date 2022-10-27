f1f2dd26927842aed3b67f5c2c6fd23505e99aff
Анатолий <contact@anafro.ru>
2022-10-27 17:29:01 +1000

    🔥 Renamed running instruction parameter

--- src/main/java/ru/anafro/quark/server/databases/ql/ConstructorEvaluator.java
+++ src/main/java/ru/anafro/quark/server/databases/ql/ConstructorEvaluator.java
@@ -6,6 +6,6 @@ import ru.anafro.quark.server.databases.ql.entities.Entity;
 public final class ConstructorEvaluator {
     public static Entity eval(String entityToEval) {
         Quark.server().getInstructionParser().parse(Quark.server().getInstructionLexer().lex("eval " + entityToEval + ";"));
-        return Quark.server().getInstructionParser().getArguments().get("object");
+        return Quark.server().getInstructionParser().getArguments().get("entity");
     }
 }
