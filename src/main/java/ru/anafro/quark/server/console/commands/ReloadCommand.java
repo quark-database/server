5c69a3ec7bf276e7aa72e78aefff1c3dc4fa3ae4
Анатолий <contact@anafro.ru>
2022-10-29 19:01:51 +1000

    🔥 Rewritten

--- src/main/java/ru/anafro/quark/server/console/commands/ReloadCommand.java
+++ src/main/java/ru/anafro/quark/server/console/commands/ReloadCommand.java
@@ -1,14 +1,9 @@
 package ru.anafro.quark.server.console.commands;
 
-import ru.anafro.quark.server.api.Quark;
 import ru.anafro.quark.server.console.Command;
 import ru.anafro.quark.server.console.CommandArguments;
 import ru.anafro.quark.server.utils.containers.UniqueList;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
+import ru.anafro.quark.server.utils.runtime.ApplicationQuitter;
 
 public class ReloadCommand extends Command {
     public ReloadCommand() {
@@ -16,22 +11,9 @@ public class ReloadCommand extends Command {
     }
 
     @Override
-    public void action(CommandArguments arguments) {  // TODO: Rewrite
-        try {
-            String javaBinaryFolder = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-            File currentJarFile = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+    public void action(CommandArguments arguments) {
+        logger.info("Reloading Quark Server...");
 
-            if(!currentJarFile.getName().endsWith(".jar")) {
-                error("The server cannot be reloaded, because it's launching file does not have the .jar extension somehow.");
-            }
-
-            final ProcessBuilder processBuilder = new ProcessBuilder(List.of(javaBinaryFolder, "-jar", currentJarFile.getPath()));
-
-            Quark.server().stop();
-            processBuilder.start();
-            System.exit(0);
-        } catch (URISyntaxException | IOException e) {
-            error("Server cannot be reloaded, because of %s: %s".formatted(e.getClass().getSimpleName(), e.getMessage()));
-        }
+        ApplicationQuitter.quit(ApplicationQuitter.ApplicationQuitterStatus.RELOAD);
     }
 }
