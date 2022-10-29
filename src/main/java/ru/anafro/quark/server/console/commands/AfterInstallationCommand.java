bcec9586f0e8add5db4331c8c3d81660312a3e0b
Анатолий <contact@anafro.ru>
2022-10-29 19:59:07 +1000

    🔥 Unified table schemes

--- src/main/java/ru/anafro/quark/server/console/commands/AfterInstallationCommand.java
+++ src/main/java/ru/anafro/quark/server/console/commands/AfterInstallationCommand.java
@@ -3,7 +3,6 @@ package ru.anafro.quark.server.console.commands;
 import ru.anafro.quark.server.api.Quark;
 import ru.anafro.quark.server.console.Command;
 import ru.anafro.quark.server.console.CommandArguments;
-import ru.anafro.quark.server.databases.data.Database;
 import ru.anafro.quark.server.databases.ql.entities.StringEntity;
 import ru.anafro.quark.server.security.Token;
 import ru.anafro.quark.server.utils.containers.UniqueList;
@@ -21,31 +20,17 @@ public class AfterInstallationCommand extends Command {
     public void action(CommandArguments arguments) {
         var generatedToken = Token.generate();
 
-        if(Database.exists("Quark")) {
-            Quark.runInstruction("""
-                    delete database "Quark";
-            """);
+        for(var scheme : Quark.tableSchemes()) {
+            logger.info("Deploying '%s'...".formatted(
+                    scheme.getTableName().getTableName()
+            ));
+
+            scheme.deploy();
         }
-
-        Quark.runInstruction("""
-                create database "Quark";
-        """);
-
-        Quark.runInstruction("""
-                create table "Quark.Tokens": columns = @list(@id(), @str("token"), @str("permission"));
-        """);
 
         Quark.runInstruction("""
                 insert into "Quark.Tokens": record = @record(:token, "*");
         """.replace(":token", new StringEntity(generatedToken).toInstructionForm()));
-
-        Quark.runInstruction("""
-                create table "Quark.Scheduled Commands": columns = @list(@id(), @str("command"), @long("period"));
-        """);
-
-        Quark.runInstruction("""
-                create table "Quark.Scheduled Queries": columns = @list(@id(), @str("query"), @long("period"));
-        """);
 
         Quark.logger().info("Your access token with all permissions: %s".formatted(generatedToken));
     }
