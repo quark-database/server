b1c279be321a0fb1a9bb711b9a305c045a261428
Anatoly Frolov <contact@anafro.ru>
2023-02-27 09:42:32 +1000

    Change the implementation

--- src/main/java/ru/anafro/quark/server/utils/containers/BTree.java
+++ src/main/java/ru/anafro/quark/server/utils/containers/BTree.java
@@ -1,22 +1,28 @@
 package ru.anafro.quark.server.utils.containers;
 
-public class BTree<Key extends Comparable<Key>, Value>  {
+import ru.anafro.quark.server.utils.strings.TextBuffer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
     // max children per B-tree node = M-1
     // (must be even and greater than 2)
-    private static final int M = 4;
+    private static final int CHILDREN_LIMIT = 4;
 
     private Node root;       // root of the B-tree
     private int height;      // height of the B-tree
-    private int n;           // number of key-value pairs in the B-tree
+    private int size;           // number of key-value pairs in the B-tree
 
     // helper B-tree node data type
     public static final class Node {
         public int childrenCount;                             // number of children
-        public Entry[] children = new Entry[M];   // the array of children
+        public Entry[] children = new Entry[CHILDREN_LIMIT];   // the array of children
 
-        // create a node with k children
-        private Node(int k) {
-            childrenCount = k;
+        private Node(int childrenCount) {
+            this.childrenCount = childrenCount;
         }
 
         public Entry[] getChildren() {
@@ -25,6 +31,23 @@ public class BTree<Key extends Comparable<Key>, Value>  {
 
         public int getChildrenCount() {
             return childrenCount;
+        }
+
+        public void traverse(Consumer consumer) {
+            for (int i = 0; i < childrenCount; i++) {
+                var child = children[i];
+                // If this is not leaf, then before printing key[i],
+                // traverse the subtree rooted with child C[i].
+                if(child != null) {
+                    if(child.getNext() != null) {
+                        child.getNext().traverse(consumer);
+                    }
+
+                    if(child.getValue() != null) {
+                        consumer.accept(child.getValue());
+                    }
+                }
+            }
         }
     }
 
@@ -73,7 +96,7 @@ public class BTree<Key extends Comparable<Key>, Value>  {
      * @return the number of key-value pairs in this symbol table
      */
     public int size() {
-        return n;
+        return size;
     }
 
     /**
@@ -94,29 +117,12 @@ public class BTree<Key extends Comparable<Key>, Value>  {
      *         and {@code null} if the key is not in the symbol table
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+    public V get(K key) {
+        if(key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+
         return search(root, key, height);
-    }
-
-    private Value search(Node x, Key key, int ht) {
-        Entry[] children = x.children;
-
-        // external node
-        if (ht == 0) {
-            for (int j = 0; j < x.childrenCount; j++) {
-                if (eq(key, children[j].key)) return (Value) children[j].value;
-            }
-        }
-
-        // internal node
-        else {
-            for (int j = 0; j < x.childrenCount; j++) {
-                if (j+1 == x.childrenCount || less(key, children[j+1].key))
-                    return search(children[j].next, key, ht-1);
-            }
-        }
-        return null;
     }
 
 
@@ -126,13 +132,17 @@ public class BTree<Key extends Comparable<Key>, Value>  {
      * If the value is {@code null}, this effectively deletes the key from the symbol table.
      *
      * @param  key the key
-     * @param  val the value
+     * @param  value the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void put(Key key, Value val) {
-        if (key == null) throw new IllegalArgumentException("argument key to put() is null");
-        Node u = insert(root, key, val, height);
-        n++;
+    public void put(K key, V value) {
+        if(key == null) {
+            throw new IllegalArgumentException("argument key to put() is null");
+        }
+
+        Node u = insert(root, key, value, height);
+        size += 1;
+
         if (u == null) return;
 
         // need to split root
@@ -142,47 +152,91 @@ public class BTree<Key extends Comparable<Key>, Value>  {
         root = t;
         height++;
     }
+    public void remove(K key) {
+        var entry = entrySearch(root, key, height);
 
-    private Node insert(Node h, Key key, Value val, int ht) {
-        int j;
-        Entry t = new Entry(key, val, null);
+        if(entry != null) {
+            entry.value = null;
+        }
+    }
+
+
+    private Entry entrySearch(Node x, K key, int ht) {
+        Entry[] children = x.children;
 
         // external node
         if (ht == 0) {
-            for (j = 0; j < h.childrenCount; j++) {
-                if (less(key, h.children[j].key)) break;
+            for (int j = 0; j < x.childrenCount; j++) {
+                if (eq(key, children[j].key)) return children[j];
             }
         }
 
         // internal node
         else {
-            for (j = 0; j < h.childrenCount; j++) {
-                if ((j+1 == h.childrenCount) || less(key, h.children[j+1].key)) {
-                    Node u = insert(h.children[j++].next, key, val, ht-1);
-                    if (u == null) return null;
-                    t.key = u.children[0].key;
-                    t.value = null;
-                    t.next = u;
+            for (int j = 0; j < x.childrenCount; j++) {
+                if (j + 1 == x.childrenCount || less(key, children[j+1].key))
+                    return entrySearch(children[j].next, key, ht-1);
+            }
+        }
+        return null;
+    }
+
+    private V search(Node x, K key, int ht) {
+        var entry = entrySearch(x, key, ht);
+        return entry == null ? null : (V) entry.value;
+    }
+
+
+    private Node insert(Node target, K key, V val, int height) {
+        int index;
+        Entry entry = new Entry(key, val, null);
+
+        if (height == 0) {  // external node
+            for (index = 0; index < target.childrenCount; index++) {
+                if (less(key, target.children[index].key)) {
+                    break;
+                }
+            }
+        } else {  // internal node
+            for (index = 0; index < target.childrenCount; index++) {
+                if (index + 1 == target.childrenCount || less(key, target.children[index + 1].key)) {
+                    Node inserted = insert(target.children[index++].next, key, val, height - 1);
+
+                    if (inserted == null) {
+                        return null;
+                    }
+
+                    entry.key = inserted.children[0].key;
+                    entry.value = null;
+                    entry.next = inserted;
+
                     break;
                 }
             }
         }
 
-        for (int i = h.childrenCount; i > j; i--)
-            h.children[i] = h.children[i-1];
-        h.children[j] = t;
-        h.childrenCount++;
-        if (h.childrenCount < M) return null;
-        else         return split(h);
+        for (int i = target.childrenCount; i > index; i--) {
+            target.children[i] = target.children[i - 1];
+        }
+
+        target.children[index] = entry;
+        target.childrenCount++;
+
+        if (target.childrenCount < CHILDREN_LIMIT) {
+            return null;
+        } else {
+            return split(target);
+        }
     }
 
     // split node in half
-    private Node split(Node h) {
-        Node t = new Node(M/2);
-        h.childrenCount = M/2;
-        for (int j = 0; j < M/2; j++)
-            t.children[j] = h.children[M/2+j];
-        return t;
+    private Node split(Node target) {
+        Node node = new Node(CHILDREN_LIMIT / 2);
+        target.childrenCount = CHILDREN_LIMIT / 2;
+
+        System.arraycopy(target.children, 2, node.children, 0, CHILDREN_LIMIT / 2);
+
+        return node;
     }
 
     /**
@@ -194,22 +248,57 @@ public class BTree<Key extends Comparable<Key>, Value>  {
         return toString(root, height, "") + "\n";
     }
 
-    private String toString(Node h, int ht, String indent) {
-        StringBuilder s = new StringBuilder();
+    private String toString(Node h, int height, String indent) {
+        var buffer = new TextBuffer();
         Entry[] children = h.children;
 
-        if (ht == 0) {
-            for (int j = 0; j < h.childrenCount; j++) {
-                s.append(indent + children[j].key + " " + children[j].value + "\n");
+        if(height == 0) {
+            for(int j = 0; j < h.childrenCount; j++) {
+                buffer.append(indent, children[j].key, " ", children[j].value, "\n");
+            }
+        } else {
+            for (int i = 0; i < h.childrenCount; i++) {
+                if(i > 0) {
+                    buffer.append(indent + "(" + children[i].key + ")\n");
+                }
+
+                buffer.append(toString(children[i].next, height-1, indent + "     "));
             }
         }
-        else {
-            for (int j = 0; j < h.childrenCount; j++) {
-                if (j > 0) s.append(indent + "(" + children[j].key + ")\n");
-                s.append(toString(children[j].next, ht-1, indent + "     "));
-            }
+
+        return buffer.toString();
+    }
+
+    public void traverse(Consumer<V> consumer) {
+        if(root != null) {
+            root.traverse(consumer);
         }
-        return s.toString();
+    }
+
+    public <A> A accumulate(A accumulator, BiConsumer<V, A> accumulationAction) {
+        traverse(value -> accumulationAction.accept(value, accumulator));
+        return accumulator;
+    }
+
+    public ArrayList<V> toList() {
+        return accumulate(Lists.empty(), (value, list) -> list.add(value));
+    }
+
+    @Override
+    public Iterator<V> iterator() {
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size();
+            }
+
+            @Override
+            public V next() {
+                return null;
+            }
+        };
     }
 
 
