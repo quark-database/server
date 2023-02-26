a0ab68f94e25d2c566dea5b90e4af7dbe7d97689
Anatoly Frolov <contact@anafro.ru>
2023-02-27 09:42:51 +1000

    Change the call stack depth

--- src/main/java/ru/anafro/quark/server/utils/exceptions/NotImplementedException.java
+++ src/main/java/ru/anafro/quark/server/utils/exceptions/NotImplementedException.java
@@ -5,6 +5,6 @@ import ru.anafro.quark.server.utils.debug.CallStack;
 
 public class NotImplementedException extends QuarkException {
     public NotImplementedException() {
-        super("Method %s is not implemented yet.".formatted(CallStack.ofDepth(2)));
+        super("Method %s is not implemented yet.".formatted(CallStack.ofDepth(3)));
     }
 }
