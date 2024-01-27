d782564a4706aa68d0e6eb9d8bce1cf6ffe55590
Anatoly Frolov <contact@anafro.ru>
2024-01-28 01:01:40 +1200

    Extract `CommandParameterType.BOOLEAN` check to a util

--- src/main/java/ru/anafro/quark/server/console/CommandParameterType.java
+++ src/main/java/ru/anafro/quark/server/console/CommandParameterType.java
@@ -1,15 +1,16 @@
 package ru.anafro.quark.server.console;
 
 import ru.anafro.quark.server.console.exceptions.CommandArgumentTypeDoesNotHaveIsSuitableValueFunctionDefinedException;
+import ru.anafro.quark.server.utils.types.Booleans;
 import ru.anafro.quark.server.utils.validation.Validators;
 
 import java.util.function.Function;
 
 public enum CommandParameterType {
-    STRING(value -> true),
+    STRING(_ -> true),
     INTEGER(Validators.INTEGER_STRING::isValid),
     FLOAT(Validators.FLOAT_STRING::isValid),
-    BOOLEAN(value -> value.equals("true") || value.equals("false"));
+    BOOLEAN(Booleans::canBeCreatedFromString);
 
     private final Function<String, Boolean> isValueSuitableFunction;
 
@@ -18,14 +19,14 @@ public enum CommandParameterType {
     }
 
     public boolean isValueSuitable(String value) {
-        if(!isValueSuitableFunctionDefined()) {
+        if (!isValueSuitableFunctionDefined()) {
             throw new CommandArgumentTypeDoesNotHaveIsSuitableValueFunctionDefinedException(this);
         }
 
         return isValueSuitableFunction.apply(value);
     }
 
-    public boolean isValueSuitableFunctionDefined() {
+    public boolean isValueSuitableFunctionDefined() {  // TODO: whats this??
         return isValueSuitableFunction != null;
     }
 }
