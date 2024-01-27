9f26f47ebfe0d12910be2eaeee1643b51d0f0e3a
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:58:26 +1200

    Add coloring and prompting to `Console`

--- src/main/java/ru/anafro/quark/server/console/Console.java
+++ src/main/java/ru/anafro/quark/server/console/Console.java
@@ -1,22 +1,70 @@
 package ru.anafro.quark.server.console;
 
-import ru.anafro.quark.server.utils.exceptions.CallingUtilityConstructorException;
+import ru.anafro.quark.server.utils.exceptions.UtilityClassInstantiationException;
+import ru.anafro.quark.server.utils.types.Booleans;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public final class Console {
+    private static final PrintStream stream = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+    private static final Scanner scanner = new Scanner(System.in);
+
     private Console() {
-        throw new CallingUtilityConstructorException(getClass());
+        throw new UtilityClassInstantiationException(getClass());
     }
 
-    public static void synchronizedPrintln(String message) {
-        synchronized(System.out) {
-            System.out.println(message);
+    public static void println(String message) {
+        synchronized (stream) {
+            stream.println(paint(message));
         }
     }
 
-    public static void synchronizedPrint(String message) {
-        synchronized(System.out) {
-            System.out.print(message);
+    public static void print(String message) {
+        synchronized (stream) {
+            stream.print(paint(message));
         }
+    }
+
+    public static void breakLine() {
+        println("");
+    }
+
+    public static void clear() {
+        println("\n".repeat(200));
+    }
+
+    public static String prompt(String promptMessage, Predicate<String> condition) {
+        String prompt;
+
+        do {
+            Console.print(STR."\{promptMessage} > <gray>");
+            prompt = scanner.nextLine();
+            Console.print("</>");
+        } while (!Objects.isNull(condition) && !condition.test(prompt));
+
+        return prompt;
+    }
+
+    public static String prompt(String promptMessage) {
+        return prompt(promptMessage, null);
+    }
+
+    public static boolean promptBoolean(String promptMessage) {
+        return Booleans.createFromString(prompt(STR."\{promptMessage} <yellow>(y / n)</>", Booleans::canBeCreatedFromString));
+    }
+
+    private static String paint(String message) {
+        var coloredMessage = message;
+
+        for (var color : ConsoleColor.values()) {
+            coloredMessage = coloredMessage.replace(STR."<\{color.name().toLowerCase()}>", color.getAnsi());
+        }
+
+        return coloredMessage.replace("</>", ConsoleColor.RESET.getAnsi());
     }
 }
 
