060c0bbb8bea723da8b7f020c4e2b2f9653f4485
Anatoly Frolov <contact@anafro.ru>
2024-01-28 01:18:55 +1200

    Remove UI exceptions

--- src/main/java/ru/anafro/quark/server/debug/exceptions/ImageForDebugFrameIconNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.anafro.quark.server.debug.exceptions;
-
-import static ru.anafro.quark.server.utils.strings.Wrapper.quoted;
-
-public class ImageForDebugFrameIconNotFoundException extends UIException {
-    public ImageForDebugFrameIconNotFoundException(String path) {
-        super("An image for debug frame icon by path %s cannot be found".formatted(quoted(path)));
-    }
-}
--- src/main/java/ru/anafro/quark/server/debug/exceptions/UIException.java
+++
@@ -1,9 +0,0 @@
-package ru.anafro.quark.server.debug.exceptions;
-
-import ru.anafro.quark.server.exceptions.QuarkException;
-
-public class UIException extends QuarkException {
-    public UIException(String message) {
-        super(message);
-    }
-}
