71d631857d6035f9f80996be5bbdd4197811ecf9
Anatoly Frolov <contact@anafro.ru>
2024-01-27 12:46:41 +1200

    Create `FileException`

---
+++ src/main/java/ru/anafro/quark/server/utils/files/exceptions/FileException.java
@@ -0,0 +1,10 @@
+package ru.anafro.quark.server.utils.files.exceptions;
+
+import ru.anafro.quark.server.utils.exceptions.UtilityException;
+
+public class FileException extends UtilityException {
+    
+    public FileException(String message) {
+        super(message);
+    }
+}
