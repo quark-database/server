9c74a884ff00b7343b8a761e0ccb2bf9804db86f
Anatoly Frolov <contact@anafro.ru>
2024-01-27 13:01:41 +1200

    Change `Counter`'s action name: `count` → `increment`

--- src/main/java/ru/anafro/quark/server/utils/integers/Counter.java
+++ src/main/java/ru/anafro/quark/server/utils/integers/Counter.java
@@ -3,17 +3,22 @@ package ru.anafro.quark.server.utils.integers;
 public class Counter {
     private int count = 0;
 
-    public void count() {
+    public void increment() {
         count++;
     }
 
-    public void countIf(boolean condition) {
-        if(condition) {
-            count();
+    public void incrementIf(boolean condition) {
+        if (condition) {
+            increment();
         }
     }
 
     public int getCount() {
         return count;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(count);
     }
 }
