2d268518417e02c3bec562e76aabeb030d52baad
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:28:03 +1200

    Change byte count type from `long` to `int`

--- src/main/java/ru/anafro/quark/server/utils/types/exceptions/WrongByteCountException.java
+++ src/main/java/ru/anafro/quark/server/utils/types/exceptions/WrongByteCountException.java
@@ -6,23 +6,20 @@ import ru.anafro.quark.server.utils.types.Bytes;
  * {@link WrongByteCountException} used only inside {@link Bytes} class
  * when bytes cannot be converted to the desired type.
  *
- * @since  Quark 1.1
  * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
- * @see    Bytes
+ * @see Bytes
+ * @since Quark 1.1
  */
 public class WrongByteCountException extends TypeException {
 
     /**
      * Creates an exception instance.
      *
-     * @param bytesNeeded     bytes count that was needed for a type.
-     * @param actualByteCount bytes count that was received instead.
-     *
-     * @since  Quark 1.1
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     * @see    Bytes
+     * @see Bytes
+     * @since Quark 1.1
      */
-    public WrongByteCountException(long bytesNeeded, long actualByteCount) {
-        super("Byte count is wrong: " + bytesNeeded + " needed, but there are " + actualByteCount);
+    public WrongByteCountException(int bytesNeeded, int actualByteCount) {
+        super(STR."Byte count is wrong: \{bytesNeeded} needed, but there are \{actualByteCount}");
     }
 }
