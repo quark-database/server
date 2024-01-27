cfebe600109936ec604673bb950d1ea06f6337cb
Anatoly Frolov <contact@anafro.ru>
2024-01-27 13:05:36 +1200

    Add `ExitCodes` constants set

---
+++ src/main/java/ru/anafro/quark/server/utils/runtime/ExitCodes.java
@@ -0,0 +1,12 @@
+package ru.anafro.quark.server.utils.runtime;
+
+
+public final class ExitCodes {
+    public static final int OK = 0;
+    public static final int ERROR = 1;
+    public static final int RELOAD = 2;
+
+
+    private ExitCodes() {
+    }
+}
