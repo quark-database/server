8ad484bdc5b8c59779bb23b445bf5bd8aba24035
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:53:31 +1200

    Use string templates

--- src/main/java/ru/anafro/quark/server/console/exceptions/CommandArgumentTypeDoesNotHaveIsSuitableValueFunctionDefinedException.java
+++ src/main/java/ru/anafro/quark/server/console/exceptions/CommandArgumentTypeDoesNotHaveIsSuitableValueFunctionDefinedException.java
@@ -4,6 +4,6 @@ import ru.anafro.quark.server.console.CommandParameterType;
 
 public class CommandArgumentTypeDoesNotHaveIsSuitableValueFunctionDefinedException extends CommandException {
     public CommandArgumentTypeDoesNotHaveIsSuitableValueFunctionDefinedException(CommandParameterType type) {
-        super("Command argument type " + type.name() + " was told to check the validity of value, but there's no 'isValueSuitableFunction' set inside this type");
+        super(STR."Command argument type \{type.name()} was told to check the validity of value, but there's no 'isValueSuitableFunction' set inside this type");
     }
 }
