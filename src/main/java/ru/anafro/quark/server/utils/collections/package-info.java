19f4ad3d19b120f7759fcec2222aec31172cc1b5
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:34:37 +1200

    Rearrange collection packages

---
+++ src/main/java/ru/anafro/quark/server/utils/collections/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains all the custom json structures, and also
+ * utility classes for handling the default ones.
+ *
+ * @version Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ * @since Quark 1.1
+ */
+package ru.anafro.quark.server.utils.collections;
--- src/main/java/ru/anafro/quark/server/utils/containers/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Contains all the custom data structures, and also
- * utility classes for handling the default ones.
- *
- * @since   Quark 1.1
- * @version Quark 1.1
- * @author  Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
- */
-package ru.anafro.quark.server.utils.containers;
