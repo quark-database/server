9e75f3e19d38a00ea848d3c9acb8db05c884d288
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:59:39 +1200

    Add console clean mode & Refactor

--- src/main/java/ru/anafro/quark/server/console/CommandLoop.java
+++ src/main/java/ru/anafro/quark/server/console/CommandLoop.java
@@ -1,202 +1,170 @@
 package ru.anafro.quark.server.console;
 
-import ru.anafro.quark.server.api.Quark;
-import ru.anafro.quark.server.console.exceptions.CommandException;
 import ru.anafro.quark.server.console.parser.CommandParser;
+import ru.anafro.quark.server.database.language.Query;
 import ru.anafro.quark.server.exceptions.QuarkException;
-import ru.anafro.quark.server.multithreading.AsyncService;
+import ru.anafro.quark.server.facade.Quark;
+import ru.anafro.quark.server.fun.Greeter;
+import ru.anafro.quark.server.logging.Logger;
 import ru.anafro.quark.server.multithreading.Threads;
-import ru.anafro.quark.server.networking.Server;
-import ru.anafro.quark.server.utils.strings.StringSimilarityFinder;
+import ru.anafro.quark.server.utils.strings.Strings;
 import ru.anafro.quark.server.utils.time.TimeSpan;
+import ru.anafro.quark.server.utils.types.classes.Enums;
 
-import java.util.Scanner;
+import java.util.Optional;
+
+import static ru.anafro.quark.server.database.language.ResponseStatus.OK;
+import static ru.anafro.quark.server.logging.LoggingLevel.ERROR;
+import static ru.anafro.quark.server.logging.LoggingLevel.INFO;
 
 /**
  * Represents the command loop reading commands
  * from the console line. To get default command loop of Quark,
  * use: {@code Quark.commandLoop();}
  *
- * @since   Quark 1.1
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
  * @version Quark 1.1
- * @author  Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ * @since Quark 1.1
  */
-public class CommandLoop extends AsyncService {
+public class CommandLoop implements Runnable {
 
     /**
      * The delay in seconds to wait before running command loops.
+     *
      * @since Quark 1.1
      */
     private static final TimeSpan DELAY_BEFORE_RUNNING = TimeSpan.milliseconds(600);
 
     /**
-     * The scanner that receives input from the console line.
-     * @since Quark 1.1
-     */
-    private final Scanner scanner = new Scanner(System.in);
-
-    /**
      * The parser that handles commands received.
+     *
      * @since Quark 1.1
      */
     private final CommandParser parser = new CommandParser();
-
-    /**
-     * The server that commands can use inside their actions.
-     * @deprecated This field will be removed soon.
-     *             Use {@code Quark.server()} instead.
-     * @since Quark 1.1
-     */
-    @Deprecated
-    private final Server server;
-
-    /**
-     * Set this field to {@code true} to make the current
-     * command reading the last one for this loop.
-     * @since Quark 1.1
-     */
-    private boolean isReadingNextCommandsStopped;
-
     private final String commandPrefix = "/";
-
-    /**
-     * Creates a new command loop. It does not run by default.
-     * To run it, use {@link CommandLoop#run()}.
-     *
-     * @param server the server.
-     *
-     * @since  Quark 1.1
-     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     *
-     * @deprecated The {@code server} argument is going to be removed.
-     */
-    @Deprecated
-    public CommandLoop(Server server) {
-        super("command-loop");
-        this.server = server;
-    }
-
-    /**
-     * It waits for 600 milliseconds, then it starts
-     * reading commands from the console line.
-     *
-     * @since  Quark 1.1
-     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     */
-    public void startReadingCommands() {
-        Threads.freezeFor(DELAY_BEFORE_RUNNING);
-
-        while(!isReadingNextCommandsStopped()) {
-            Console.synchronizedPrint("Quark Server > ");
-            var input = scanner.nextLine().strip();
-
-            if(input.startsWith(commandPrefix)) {
-                try {
-                    Quark.runCommand(input.substring(commandPrefix.length()));
-                } catch(CommandException exception) {
-                    Quark.logger().error(exception.getMessage());
-                }
-            } else if(!input.isBlank()) {
-                try {
-                    Quark.info(Quark.runInstruction(input).toString());
-                } catch(QuarkException exception) {
-                    Quark.error(exception.getMessage());
-                }
-            }
-        }
-    }
-
-    /**
-     * @return the server of this command loop.
-     *
-     * @since  Quark 1.1
-     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     *
-     * @deprecated This getter will be removed soon.
-     *             Use {@code Quark.server()} instead.
-     */
-    @Deprecated
-    public Server getServer() {
-        return server;
-    }
-
-    /**
-     * @return {@code true} if reading the next commands is stopped.
-     *         Otherwise, this method returns {@code false}.
-     *
-     * @since  Quark 1.1
-     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     */
-    public boolean isReadingNextCommandsStopped() {
-        return isReadingNextCommandsStopped;
-    }
-
-    /**
-     * It finds the most similar command name to the one that was passed in
-     * 
-     * @param notExistingCommandName the command name that doesn't exist.
-     * @return the name of the command that is most similar to the command that was not found.
-     *
-     * @since Quark 1.1
-     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     *
-     * @deprecated Use {@link CommandRegistry#suggest(String)} from {@link Quark#commands()} instead.
-     */
-    @Deprecated
-    public String suggestCommand(String notExistingCommandName) {
-        if(Quark.commands().count() == 0) {
-            return null;
-        }
-
-        String suggestedCommand = Quark.commands().asList().get(0).getPrimaryName();
-        double similarity = Double.NEGATIVE_INFINITY;
-
-        for(var command : Quark.commands()) {
-            for(var commandName : command.getNames()) {
-                double currentSimilarity = StringSimilarityFinder.findSimilarity(notExistingCommandName, commandName);
-
-                if(similarity <= currentSimilarity) {
-                    suggestedCommand = commandName;
-                    similarity = currentSimilarity;
-                }
-            }
-        }
-
-        return suggestedCommand;
-    }
-
-    /**
-     * Stops reading the next commands. It means that
-     * the command loop will not stop reading current command,
-     * but it will stop reading the next ones.
-     *
-     * @since  Quark 1.1
-     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     */
-    public void stopReadingNextCommands() {
-        this.isReadingNextCommandsStopped = true;
-    }
+    private final Logger logger = new Logger(getClass());
+    private String failedCommand = null;
+    private CommandArguments failedArguments = null;
+    private boolean isRunning = false;
+    private boolean isCleanModeOn = false;
 
     /**
      * @return the parser that this command loop uses
-     *         to parse read commands.
-     *
-     * @since  Quark 1.1
+     * to parse read commands.
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
     public CommandParser getParser() {
         return parser;
     }
 
     /**
-     * Runs reading commands. This method is an alias
-     * for {@link CommandLoop#startReadingCommands()}.
+     * Starts reading commands from the console line.
      *
-     * @since  Quark 1.1
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
     @Override
     public void run() {
-        startReadingCommands();
+        Threads.sleepFor(DELAY_BEFORE_RUNNING);
+        Greeter.greet();
+
+        this.isRunning = true;
+
+        while (this.isRunning) {
+            var prompt = Console.prompt("Quark").transform(this::preparePrompt);
+
+            try {
+                if (isCleanModeOn) {
+                    Console.clear();
+                }
+
+                if (isCommand(prompt)) {
+                    var commandPrompt = prepareCommand(prompt);
+                    var parser = Quark.commandLoop().getParser();
+                    var command = parser.parse(commandPrompt);
+                    var arguments = parser.getArguments();
+
+                    command.run(arguments);
+                    forgetFailedCommand();
+                }
+
+                if (isQuery(prompt)) {
+                    var query = Query.make(prompt);
+                    var result = query.execute();
+                    var instruction = query.instruction();
+                    var arguments = query.arguments();
+                    var status = result.responseStatus();
+                    var message = result.message();
+                    var view = result.tableView();
+                    var time = result.milliseconds();
+
+                    logger.log(status == OK ? INFO : ERROR, STR."""
+                            Executed a query:
+                            \{instruction.format(arguments)}
+
+                            \{Enums.getDisplayName(status)}: \{message}
+                            \{view}
+
+                            Done in \{time} ms
+                            """);
+                }
+            } catch (QuarkException exception) {
+                Quark.error(exception.getMessage());
+            }
+        }
+    }
+
+    public String getCommandPrefix() {
+        return commandPrefix;
+    }
+
+    private String preparePrompt(String prompt) {
+        return prompt.strip();
+    }
+
+    private String prepareCommand(String prompt) {
+        return Strings.removeLeading(prompt, commandPrefix);
+    }
+
+    private boolean isCommand(String prompt) {
+        return prompt.startsWith(commandPrefix);
+    }
+
+    private boolean isQuery(String prompt) {
+        return !isCommand(prompt) && !prompt.isBlank();
+    }
+
+    public Optional<String> tryGetFailedCommand() {
+        return Optional.ofNullable(failedCommand);
+    }
+
+    public void setFailedCommand(String failedCommand) {
+        this.failedCommand = failedCommand;
+    }
+
+    public void forgetFailedCommand() {
+        this.failedCommand = null;
+        this.failedArguments = null;
+    }
+
+    public CommandArguments getFailedArguments() {
+        return failedArguments;
+    }
+
+    public void setFailedArguments(CommandArguments failedArguments) {
+        this.failedArguments = failedArguments;
+    }
+
+    public void toggleCleanMode() {
+        isCleanModeOn = !isCleanModeOn;
+    }
+
+    public boolean isCleanModeOn() {
+        return isCleanModeOn;
+    }
+
+    public void stop() {
+        this.isRunning = false;
     }
 }
