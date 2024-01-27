b9f4f2511aed8cdf8ab86ccfc5a82f4fb247e0f8
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:22:23 +1200

    Added `L` suffix to the integers

--- src/main/java/ru/anafro/quark/server/utils/time/TimeUnit.java
+++ src/main/java/ru/anafro/quark/server/utils/time/TimeUnit.java
@@ -2,13 +2,14 @@ package ru.anafro.quark.server.utils.time;
 
 
 public enum TimeUnit {
-    MILLISECONDS(1),
-    SECONDS(1000),
-    MINUTES(60_000),
-    HOURS(360_000),
-    DAYS(8_640_000),
-    MONTHS(259_200_000),
-    YEARS(3_153_600_000);
+    MILLISECONDS(1L),
+    SECONDS(1000L),
+    MINUTES(60_000L),
+    HOURS(360_000L),
+    DAYS(8_640_000L),
+    WEEKS(60_480_000L),
+    MONTHS(259_200_000L),
+    YEARS(3_153_600_000L);
 
     final long milliseconds;
 
