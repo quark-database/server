2d3c6f6854e1bd2a9788fe2704bba99626a6335d
Anatoly Frolov <contact@anafro.ru>
2024-01-27 12:43:30 +1200

    Add `tryGet`, 'indexOfKey' utility & Extract delimiter

--- src/main/java/ru/anafro/quark/server/utils/collections/Lists.java
+++ src/main/java/ru/anafro/quark/server/utils/collections/Lists.java
@@ -1,31 +1,34 @@
-package ru.anafro.quark.server.utils.containers;
+package ru.anafro.quark.server.utils.collections;
 
-import ru.anafro.quark.server.utils.exceptions.CallingUtilityConstructorException;
+import ru.anafro.quark.server.utils.exceptions.UtilityClassInstantiationException;
 import ru.anafro.quark.server.utils.strings.TextBuffer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
  * Lists class contains uncategorized utility methods for
  * list manipulating. See static methods descriptions to learn more.
  *
- * @since  Quark 1.1
  * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
- * @see    Lists#join(List)
+ * @see Lists#join(List)
+ * @since Quark 1.1
  */
 public final class Lists {
+
+    private static final String DEFAULT_DELIMITER = ", ";
 
     /**
      * This private constructor of Lists class <strong>MUST NOT</strong> be ever
      * called, because Lists is a utility class. Use static methods declared inside.
      *
-     * @since  Quark 1.1
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
     private Lists() {
-        throw new CallingUtilityConstructorException(getClass());
+        throw new UtilityClassInstantiationException(getClass());
     }
 
     /**
@@ -42,19 +45,18 @@ public final class Lists {
      *
      * @param collection a list to join.
      * @param separator  a separator that will be put between each element, <i>but not at the end of the string</i>.
-     * @return           a list joined with a separator between each element.
      * @param <T>        a type of list.
-     *
-     * @since  Quark 1.1
+     * @return a list joined with a separator between each element.
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
     public static <T> String join(List<? extends T> collection, String separator) {
         TextBuffer joinedContainer = new TextBuffer();
 
-        for(int index = 0; index < collection.size(); index++) {
-            joinedContainer.append(collection.get(index));
+        for (int index = 0; index < collection.size(); index++) {
+            joinedContainer.append(collection.get(index).toString());
 
-            if(index != collection.size() - 1) {
+            if (index != collection.size() - 1) {
                 joinedContainer.append(separator);
             }
         }
@@ -66,21 +68,32 @@ public final class Lists {
      * Joins a list with a comma as a separator between each element.
      *
      * @param collection a list to join.
-     * @return           a list joined with a comma between each element.
      * @param <T>        a list type.
-     *
-     * @since  Quark 1.1
+     * @return a list joined with a comma between each element.
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
     public static <T> String join(List<? extends T> collection) {
-        return join(collection, ", ");
+        return join(collection, DEFAULT_DELIMITER);
     }
 
     public static <T> ArrayList<T> empty() {
         return new ArrayList<>();
     }
 
-    public static <T> String joinPresentations(List<? extends T> collection, Function<T, String> presentation) {
+    public static <T> String join(List<? extends T> collection, Function<T, String> presentation) {
         return join(collection.stream().map(presentation).toList());
+    }
+
+    public static <T> Optional<T> tryGet(ArrayList<T> list, int index) {
+        if (Collections.isInvalidIndex(list, index)) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(list.get(index));
+    }
+
+    public static <T, K> int indexOfKey(List<T> list, K key, Function<T, ?> keyGetter) {
+        return list.stream().map(keyGetter).toList().indexOf(key);
     }
 }
