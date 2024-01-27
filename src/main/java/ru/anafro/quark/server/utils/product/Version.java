9ce89a978ec002371518ec4816a32740156eda7f
Anatoly Frolov <contact@anafro.ru>
2024-01-27 13:04:29 +1200

    Make `Version` a record

--- src/main/java/ru/anafro/quark/server/utils/product/Version.java
+++ src/main/java/ru/anafro/quark/server/utils/product/Version.java
@@ -7,56 +7,15 @@ import ru.anafro.quark.server.utils.strings.TextBuffer;
  * releasing. Put it into the main class of your
  * application.
  *
- * @since   Quark 1.1
+ * @param major       The major part of the version.
+ * @param minor       The minor part of the version.
+ * @param patch       The patch of the version.
+ * @param releaseType The release type of the version.
+ * @author Anatoly Frolov
  * @version Quark 2.0
- * @author  Anatoly Frolov
+ * @since Quark 1.1
  */
-public class Version {
-    /**
-     * No release type is used to indicate that
-     * the version does not have the release type.
-     * Do not use it implicitly, use the constructor
-     * without the release type instead.
-     *
-     * @since   Quark 2.0
-     */
-    public static final String NO_RELEASE_TYPE = null;
-
-    /**
-     * This is the string that will be between the version parts
-     * when {@link Version#toString()} is invoked.
-     *
-     * @since   Quark 2.0
-     */
-    public static final String VERSION_PARTS_DELIMITER = ".";
-
-    /**
-     * The major part of the version.
-     *
-     * @since   Quark 1.1
-     */
-    private final int major;
-
-    /**
-     * The minor part of the version.
-     *
-     * @since   Quark 1.1
-     */
-    private final int minor;
-
-    /**
-     * The patch of the version.
-     *
-     * @since   Quark 2.0
-     */
-    private final int patch;
-
-    /**
-     * The release type of the version.
-     *
-     * @since   Quark 2.0
-     */
-    private final String releaseType;
+public record Version(int major, int minor, int patch, String releaseType) {
 
     /**
      * Creates a new version object with major, minor, and patch
@@ -69,106 +28,44 @@ public class Version {
      * If the application is a release, please, use a constructor
      * without the {@code releaseType} parameter.
      *
-     * @param   major       The major part of the version.
-     * @param   minor       The minor part of the version.
-     * @param   patch       The patch of the version.
-     * @param   releaseType The release type.
-     * @since   Quark 2.0
-     * @author  Anatoly Frolov <contact@anafro.ru>
+     * @param major       The major part of the version.
+     * @param minor       The minor part of the version.
+     * @param patch       The patch of the version.
+     * @param releaseType The release type.
+     * @author Anatoly Frolov <contact@anafro.ru>
+     * @since Quark 2.0
      */
-    public Version(int major, int minor, int patch, String releaseType) {
-        this.major = major;
-        this.minor = minor;
-        this.patch = patch;
-        this.releaseType = releaseType;
-    }
-
-    /**
-     * Creates a new version object with major, minor, and patch
-     * parts.
-     *
-     * @param   major       The major part of the version.
-     * @param   minor       The minor part of the version.
-     * @param   patch       The patch of the version.
-     * @since   Quark 2.0
-     * @author  Anatoly Frolov <contact@anafro.ru>
-     */
-    public Version(int major, int minor, int patch) {
-        this(major, minor, patch, NO_RELEASE_TYPE);
+    public Version {
     }
 
     /**
      * Returns {@code true} if the version has a release type.
      * Otherwise, the method will return {@code false}.
      *
-     * @since   Quark 2.0
-     * @author  Anatoly Frolov <contact@anafro.ru>
-     * @return  Does the version have the release type.
+     * @return Does the version have the release type.
+     * @author Anatoly Frolov <contact@anafro.ru>
+     * @since Quark 2.0
      */
     public boolean hasReleaseType() {
         return releaseType != null;
     }
 
-    /**
-     * Returns the major part of the version.
-     *
-     * @since  Quark 2.0
-     * @author Anatoly Frolov <contact@anafro.ru>
-     * @return the major part of the version.
-     */
-    public int getMajor() {
-        return major;
-    }
-
-    /**
-     * Returns the minor part of the version.
-     *
-     * @since  Quark 2.0
-     * @author Anatoly Frolov <contact@anafro.ru>
-     * @return the minor part of the version.
-     */
-    public int getMinor() {
-        return minor;
-    }
-
-    /**
-     * Returns the patch of the version.
-     *
-     * @since  Quark 2.0
-     * @author Anatoly Frolov <contact@anafro.ru>
-     * @return the patch of the version.
-     */
-    public int getPatch() {
-        return patch;
-    }
-
-    /**
-     * Returns the release type of the version.
-     *
-     * @since  Quark 2.0
-     * @author Anatoly Frolov <contact@anafro.ru>
-     * @return the release type of the version.
-     */
-    public String getReleaseType() {
-        return releaseType;
+    public String getShortVersion() {
+        return STR."\{major}.\{minor}.\{patch}";
     }
 
     /**
      * Converts the version object to a string.
      *
-     * @since   Quark 2.0
-     * @author  Anatoly Frolov <contact@anafro.ru>
-     * @return  the string representation of the version.
+     * @return the string representation of the version.
+     * @author Anatoly Frolov <contact@anafro.ru>
+     * @since Quark 2.0
      */
     @Override
     public String toString() {
-        var version = new TextBuffer();
+        var version = new TextBuffer(getShortVersion());
 
-        version.append(major, VERSION_PARTS_DELIMITER);
-        version.append(minor, VERSION_PARTS_DELIMITER);
-        version.append(patch);
-
-        if(hasReleaseType()) {
+        if (hasReleaseType()) {
             version.append(" ", releaseType);
         }
 
