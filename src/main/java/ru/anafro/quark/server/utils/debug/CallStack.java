7640b43537361242819e92bce02bd61a5d27a50b
Anatoly Frolov <contact@anafro.ru>
2024-01-27 12:45:12 +1200

    Use string template

--- src/main/java/ru/anafro/quark/server/utils/debug/CallStack.java
+++ src/main/java/ru/anafro/quark/server/utils/debug/CallStack.java
@@ -1,17 +1,17 @@
 package ru.anafro.quark.server.utils.debug;
 
-import ru.anafro.quark.server.utils.exceptions.CallingUtilityConstructorException;
+import ru.anafro.quark.server.utils.exceptions.UtilityClassInstantiationException;
 import ru.anafro.quark.server.utils.integers.Integers;
 
 public final class CallStack {
     private CallStack() {
-        throw new CallingUtilityConstructorException(getClass());
+        throw new UtilityClassInstantiationException(getClass());
     }
 
     public static String ofDepth(int depth) {
         StackTraceElement[] elements = Thread.currentThread().getStackTrace();
         var caller = elements[Integers.limit(depth, 0, elements.length - 1)];
 
-        return caller.getClassName().substring(caller.getClassName().lastIndexOf('.') + 1) + "." + caller.getMethodName() + "(), line " + caller.getLineNumber();
+        return STR."\{caller.getClassName().substring(caller.getClassName().lastIndexOf('.') + 1)}.\{caller.getMethodName()}(), line \{caller.getLineNumber()}";
     }
 }
