71429ad334aba24b6b1f4aeff0527af330d7ce39
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:18:36 +1200

    Use string template

--- src/main/java/ru/anafro/quark/server/utils/strings/exceptions/ObjectFormatException.java
+++ src/main/java/ru/anafro/quark/server/utils/strings/exceptions/ObjectFormatException.java
@@ -9,9 +9,9 @@ import static ru.anafro.quark.server.utils.strings.Wrapper.quoted;
  * An {@link ObjectFormatException} should be thrown only from {@link Converter}
  * class to indicate that string cannot be converted to some type.
  *
- * @since  Quark 1.1
  * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
- * @see    Converter
+ * @see Converter
+ * @since Quark 1.1
  */
 public class ObjectFormatException extends QuarkException {
 
@@ -20,11 +20,10 @@ public class ObjectFormatException extends QuarkException {
      *
      * @param string a string that was tried to be converted to the specified class.
      * @param clazz  a class of type that this string was tried to be converted to.
-     *
-     * @since  Quark 1.1
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
     public ObjectFormatException(String string, Class<?> clazz) {
-        super("String " + quoted(string) + " is being tried to be converted to type " + quoted(clazz.getSimpleName()) + ", but it is not in a good format.");
+        super(STR."String \{quoted(string)} is being tried to be converted to type \{quoted(clazz.getSimpleName())}, but it is not in a good format.");
     }
 }
