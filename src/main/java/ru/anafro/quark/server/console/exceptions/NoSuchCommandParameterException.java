834aeb9976c49ba2d49fdbabc1dac139bb841ffc
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:51:58 +1200

    Remove unused console exceptions

--- src/main/java/ru/anafro/quark/server/console/exceptions/NoSuchCommandParameterException.java
+++
@@ -1,9 +0,0 @@
-package ru.anafro.quark.server.console.exceptions;
-
-import static ru.anafro.quark.server.utils.strings.Wrapper.quoted;
-
-public class NoSuchCommandParameterException extends CommandException {
-    public NoSuchCommandParameterException(String parameterName) {
-        super("Command is trying to access a parameter with name " + quoted(parameterName) + ", which does not exist");
-    }
-}
