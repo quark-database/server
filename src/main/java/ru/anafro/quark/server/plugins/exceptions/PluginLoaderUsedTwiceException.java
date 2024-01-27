340c32181e59f98c9d5fdc02ce3964574f94016e
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:36:26 +1200

    Remove unused plugin exceptions

--- src/main/java/ru/anafro/quark/server/plugins/exceptions/PluginLoaderUsedTwiceException.java
+++
@@ -1,7 +0,0 @@
-package ru.anafro.quark.server.plugins.exceptions;
-
-public class PluginLoaderUsedTwiceException extends PluginException {
-    public PluginLoaderUsedTwiceException() {
-        super("The plugin loader cannot be used twice. Remove an additional usage of loader.loadPlugins()");
-    }
-}
