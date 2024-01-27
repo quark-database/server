54602501035fe0aad504bbde1fdd90e9fbef4570
Anatoly Frolov <contact@anafro.ru>
2024-01-28 01:15:45 +1200

    Move there

---
+++ src/main/java/ru/anafro/quark/server/networking/Client.java
@@ -0,0 +1,68 @@
+package ru.anafro.quark.server.networking;
+
+import org.json.JSONObject;
+import ru.anafro.quark.server.database.language.ResponseStatus;
+import ru.anafro.quark.server.logging.Logger;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    private final Socket socket;
+    private final Logger logger = new Logger(getClass());
+
+    public Client(Socket socket) {
+        this.socket = socket;
+    }
+
+    public Request receiveRequest() {
+        try (var stream = socket.getInputStream()) {
+            return Request.createFromJson(NetworkMessage.collectString(stream));
+        } catch (IOException exception) {
+            var ip = this.getIp();
+            var port = this.getPort();
+            logger.info(STR."Receiving a request from client at \{ip}:\{port} failed: \{exception.getMessage()}");
+
+            return null;
+        }
+    }
+
+    public void send(Response response) {
+        sendMessage(response.json());
+    }
+
+    public void sendMessage(JSONObject json) {
+        var message = new NetworkMessage(json.toString());
+
+        try (var stream = new DataOutputStream(socket.getOutputStream())) {
+            stream.write(message.toByteArray());
+        } catch (IOException exception) {
+            var ip = this.getIp();
+            var port = this.getPort();
+
+            logger.info(STR."Sending a message to client at \{ip}:\{port} failed: \{exception.getMessage()}");
+        }
+    }
+
+    public void sendError(String message, ResponseStatus status) {
+        var response = new JSONObject();
+
+        response.put("status", status.name());
+        response.put("message", message);
+
+        sendMessage(response);
+    }
+
+    private String getIp() {
+        return socket.getInetAddress().getHostAddress();
+    }
+
+    private int getPort() {
+        return socket.getPort();
+    }
+
+    public void send(MiddlewareResponse middlewareResponse) {
+        sendError(middlewareResponse.getReason(), ResponseStatus.MIDDLEWARE_ERROR);
+    }
+}
