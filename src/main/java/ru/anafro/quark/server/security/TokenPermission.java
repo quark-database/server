ab2c080d243a94724459345c4d514acfe15edf2d
Anatoly Frolov <contact@anafro.ru>
2024-01-28 00:16:39 +1200

    Rename method `hasPermission` → `can`

--- src/main/java/ru/anafro/quark/server/security/TokenPermission.java
+++ src/main/java/ru/anafro/quark/server/security/TokenPermission.java
@@ -5,20 +5,16 @@ package ru.anafro.quark.server.security;
  * token. A permission is what token can actually do
  * on the server and what instructions.
  *
- * @since   Quark 1.1
+ * @param permission The permission in string.
+ * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
  * @version Quark 1.1
- * @author  Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+ * @since Quark 1.1
  */
-public class TokenPermission {
-
-    /**
-     * The permission in string.
-     * @since Quark 1.1
-     */
-    private final String permission;
+public record TokenPermission(String permission) {
 
     /**
      * The all-permissions token in string.
+     *
      * @since Quark 1.1
      */
     public static final String ALL_PERMISSIONS = "*";
@@ -28,12 +24,10 @@ public class TokenPermission {
      * Creates a new token permission object.
      *
      * @param permission the permission
-     *
-     * @since  Quark 1.1
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
-    public TokenPermission(String permission) {
-        this.permission = permission;
+    public TokenPermission {
     }
 
     /**
@@ -41,20 +35,19 @@ public class TokenPermission {
      *
      * @param permission the checking permission
      * @return {@code true} when passed permission is allowed to do what this permission allows.
-     *         Otherwise, {@code false} is returned.
-     *
-     * @since  Quark 1.1
+     * Otherwise, {@code false} is returned.
      * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
+     * @since Quark 1.1
      */
     public boolean includesPermission(String permission) {
-        if(permission.equals(ALLOWED_FOR_ALL_TOKENS)) {
+        if (permission.equals(ALLOWED_FOR_ALL_TOKENS)) {
             return true;
         }
 
         var thisPermissionEntities = this.permission.split("\\.");
         var thatPermissionEntities = permission.split("\\.");
-        for(int index = 0; index < Math.min(thatPermissionEntities.length, thisPermissionEntities.length); index++) {
-            if(!thisPermissionEntities[index].equals(thatPermissionEntities[index])) {
+        for (int index = 0; index < Math.min(thatPermissionEntities.length, thisPermissionEntities.length); index++) {
+            if (!thisPermissionEntities[index].equals(thatPermissionEntities[index])) {
                 return thatPermissionEntities[index].equals(ALL_PERMISSIONS);
             }
         }
@@ -62,12 +55,4 @@ public class TokenPermission {
         return true;
     }
 
-    /**
-     * @return the permission in string.
-     * @since  Quark 1.1
-     * @author Anatoly Frolov | Анатолий Фролов | <a href="https://anafro.ru">My website</a>
-     */
-    public String getPermission() {
-        return permission;
-    }
 }
