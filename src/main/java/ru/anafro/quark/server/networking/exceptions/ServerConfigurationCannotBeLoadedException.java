ad6aeb4c9bd7d0ef55c3dff71c066d5daf7d062f
Anatoly Frolov <contact@anafro.ru>
2024-01-28 01:06:44 +1200

    Remove unused networking exceptions

--- src/main/java/ru/anafro/quark/server/networking/exceptions/ServerConfigurationCannotBeLoadedException.java
+++
@@ -1,7 +0,0 @@
-package ru.anafro.quark.server.networking.exceptions;
-
-public class ServerConfigurationCannotBeLoadedException extends NetworkingException {
-    public ServerConfigurationCannotBeLoadedException(String path, Throwable becauseOf) {
-        super("Configuration file " + path + " cannot be loaded, because of " + becauseOf.getClass().getSimpleName() + ": " + becauseOf.getMessage());
-    }
-}
