e514a4a95dbe240eec0b951df20f83ed0e2718e9
Anatoly Frolov <contact@anafro.ru>
2024-01-28 13:47:04 +1200

    Remove redundant `FileException`

--- src/main/java/ru/anafro/quark/server/files/exceptions/FileException.java
+++
@@ -1,9 +0,0 @@
-package ru.anafro.quark.server.files.exceptions;
-
-import ru.anafro.quark.server.exceptions.QuarkException;
-
-public class FileException extends QuarkException {
-    public FileException(String message) {
-        super(message);
-    }
-}
