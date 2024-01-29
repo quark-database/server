21e00726fb260f49ea8b6b3d6da629b073a7a940
Anatoly Frolov <contact@anafro.ru>
2024-01-29 15:34:33 +1200

    Add `Enums.max` util

--- src/main/java/ru/anafro/quark/server/utils/types/classes/Enums.java
+++ src/main/java/ru/anafro/quark/server/utils/types/classes/Enums.java
@@ -2,6 +2,10 @@ package ru.anafro.quark.server.utils.types.classes;
 
 import ru.anafro.quark.server.utils.strings.Strings;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Function;
+
 import static ru.anafro.quark.server.utils.arrays.Arrays.map;
 
 public final class Enums {
@@ -15,5 +19,9 @@ public final class Enums {
 
     public static <T extends Enum<T>> String getDisplayName(T value) {
         return value.name().toLowerCase().replace('_', ' ').transform(Strings::capitalize);
+    }
+
+    public static <T extends Enum<T>, K extends Comparable<K>> T max(T[] values, Function<T, K> keyExtractor) {
+        return Arrays.stream(values).max(Comparator.comparing(keyExtractor)).orElseThrow();
     }
 }
