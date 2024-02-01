0c097bc2316a79b83a5e5ffd9c2bc4318924f021
Anatoly Frolov <contact@anafro.ru>
2024-02-01 13:14:32 +1200

    Fix `Networking` returning closed sockets

--- src/main/java/ru/anafro/quark/server/utils/networking/Networking.java
+++ src/main/java/ru/anafro/quark/server/utils/networking/Networking.java
@@ -9,19 +9,20 @@ import java.net.Socket;
 public final class Networking {
     private static final int FIND_AND_ALLOCATE_PORT = 0;
 
-    private Networking() {}
+    private Networking() {
+    }
 
     public static ServerSocket createServerSocket(int port) {
-        try (var socket = new ServerSocket(Ports.isUsable(port) ? port : FIND_AND_ALLOCATE_PORT)) {
-            return socket;
+        try {
+            return new ServerSocket(Ports.isUsable(port) ? port : FIND_AND_ALLOCATE_PORT);
         } catch (IOException exception) {
             throw new NetworkingException(exception.getMessage());
         }
     }
 
     public static Socket acceptClientSocket(ServerSocket serverSocket) {
-        try (var socket = serverSocket.accept()) {
-            return socket;
+        try {
+            return serverSocket.accept();
         } catch (IOException exception) {
             throw new NetworkingException(exception.getMessage());
         }
