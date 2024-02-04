56df345dcd76d10b6453edb7098abb65e128e2bc
Anatoly Frolov <contact@anafro.ru>
2024-02-04 18:23:59 +1200

    Add README documentation generation skeleton

---
+++ generate-docs.py
@@ -0,0 +1,16 @@
+README_TEMPLATE_PATH = "Readme-Template.md"
+README_PATH = "README.md"
+MD_DOCS_TAG = "<!-- Quark:Docs -->"
+
+
+def main():
+    with open(README_TEMPLATE_PATH, 'r') as readme_template:
+        readme_template_content = readme_template.read()
+        readme_template_content.replace(MD_DOCS_TAG, '*It works!*')
+
+    with open(README_PATH, 'w') as readme:
+        readme.write(readme_template_content)
+
+
+if __name__ == '__main__':
+    main()
