d35f2fd8a1032e815fa10d0e033661bbbe3bdfd5
Anatoly Frolov <contact@anafro.ru>
2024-02-04 18:41:05 +1200

    Set docs tag replacement to new README content

--- generate_docs.py
+++ generate_docs.py
@@ -6,7 +6,8 @@ MD_DOCS_TAG = "<!-- Quark:Docs -->"
 def main():
     with open(README_TEMPLATE_PATH, 'r') as readme_template:
         readme_template_content = readme_template.read()
-        readme_template_content.replace(MD_DOCS_TAG, '*It works!*')
+        readme_template_content = readme_template_content.replace(
+            MD_DOCS_TAG, '*It works!*')
 
     with open(README_PATH, 'w') as readme:
         readme.write(readme_template_content)
