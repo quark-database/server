ac736385d702862aeb21b37b85849464fff52a1e
Anatoly Frolov <contact@anafro.ru>
2024-02-05 22:30:26 +1200

    Add `TableRecordFinder.finder` helper

--- src/main/java/ru/anafro/quark/server/database/data/TableRecordFinder.java
+++ src/main/java/ru/anafro/quark/server/database/data/TableRecordFinder.java
@@ -4,6 +4,10 @@ import ru.anafro.quark.server.language.entities.Entity;
 
 public record TableRecordFinder(String columnName, Entity findingValue) implements RecordLambda<Boolean> {
 
+    public static TableRecordFinder finder(String columnName, Object findingValue) {
+        return new TableRecordFinder(columnName, Entity.wrap(findingValue));
+    }
+
     @Override
     public Boolean apply(TableRecord record) {
         return record.getField(columnName).getEntity().getValue().equals(findingValue.getValue());
