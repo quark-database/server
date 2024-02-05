4123ff3043024550be73ed8d78d072cc80d186f6
Anatoly Frolov <contact@anafro.ru>
2024-02-05 14:31:18 +1200

    Fix `VariableDirectory` not returning list of all variables

--- src/main/java/ru/anafro/quark/server/database/data/files/VariableDirectory.java
+++ src/main/java/ru/anafro/quark/server/database/data/files/VariableDirectory.java
@@ -7,6 +7,8 @@ import ru.anafro.quark.server.utils.files.Directory;
 import java.util.Iterator;
 import java.util.List;
 
+import static ru.anafro.quark.server.utils.files.filters.ExtensionInclusionFileFilter.withExtension;
+
 public class VariableDirectory implements Iterable<TableVariable> {
     public static final String NAME = "Variables";
     private final Table table;
@@ -14,7 +16,7 @@ public class VariableDirectory implements Iterable<TableVariable> {
 
     public VariableDirectory(Table table) {
         this.table = table;
-        this.directory = table.getDatabase().getDirectory().getDirectory(table.getName(), NAME);
+        this.directory = new Directory(table.getDatabase().getDirectory().getFilePath(table.getName(), NAME), withExtension("qvariable"));
     }
 
     public TableVariable getVariable(String variableName) {
@@ -22,7 +24,7 @@ public class VariableDirectory implements Iterable<TableVariable> {
     }
 
     public List<TableVariable> all() {
-        return directory.getDirectories().stream().map(directory -> getVariable(directory.getName())).toList();
+        return directory.files().map(file -> new TableVariable(table, file.getNameWithoutExtension())).toList();
     }
 
     public boolean hasVariable(String variableName) {
