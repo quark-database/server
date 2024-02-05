5a2e95465cd50170dca8a6787c38e56455313f93
Anatoly Frolov <contact@anafro.ru>
2024-02-05 18:01:54 +1200

    Add `RecordFieldGenerator.generator` helper

--- src/main/java/ru/anafro/quark/server/database/data/RecordFieldGenerator.java
+++ src/main/java/ru/anafro/quark/server/database/data/RecordFieldGenerator.java
@@ -6,6 +6,10 @@ import ru.anafro.quark.server.language.entities.Entity;
 public record RecordFieldGenerator(String expression) implements RecordLambda<Entity> {
     public static final String COLUMN_NAME_MARKER = ":";
 
+    public static RecordFieldGenerator generator(String expression) {
+        return new RecordFieldGenerator(expression);
+    }
+
     @Override
     public Entity apply(TableRecord record) {
         String filledExpression = expression;   // TODO: Repeated code
