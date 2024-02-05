a550912662c2f8bffeba3eac3243a0b2291b0909
Anatoly Frolov <contact@anafro.ru>
2024-02-05 15:51:12 +1200

    Add `TableViewHeader.equals`

--- src/main/java/ru/anafro/quark/server/database/views/TableViewHeader.java
+++ src/main/java/ru/anafro/quark/server/database/views/TableViewHeader.java
@@ -1,4 +1,18 @@
 package ru.anafro.quark.server.database.views;
 
+import java.util.Arrays;
+
 public record TableViewHeader(String... columnNames) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TableViewHeader that = (TableViewHeader) o;
+        return Arrays.equals(columnNames, that.columnNames);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(columnNames);
+    }
 }
